09733f5f8b976af9f4ce6a43ce83adece621ff58
Ethan Groene <ethangroene@gmail.com>
2024-04-08 22:08:27 -0600

    Rename component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import Welcome from "./Components/Welcome/Welcome";
-import Homepage from "./Components/Homepage/Homepage";
+import UserHomepage from "./Components/UserHomepage/UserHomepage";
 import { useMainContext } from "./Hooks/useMainContext";
 import { Route, Routes } from "react-router-dom";
 
@@ -25,7 +25,7 @@ function App() {
             )
           }
         />
-        <Route path="/users/:username" element={<Homepage />} />
+        <Route path="/users/:username" element={<UserHomepage />} />
       </Routes>
     </div>
   );
---
+++ src/Components/UserHomepage/UserHomepage.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useMainContext } from "../../Hooks/useMainContext";
+
+const UserHomepage = () => {
+  const { currentUser } = useMainContext();
+
+  /* If currentUser is undefined, redirect to base URL (/). This prevents access to user account by simply pasting in their acct url. Forces login. Also, this component will only render if currentUser exists */
+  const navigation = useNavigate();
+  if (!currentUser) {
+    navigation("/");
+  }
+
+  return (
+    currentUser && (
+      <>
+        <h1>Homepage</h1>
+        <p>hello</p>
+      </>
+    )
+  );
+};
+export default UserHomepage;
