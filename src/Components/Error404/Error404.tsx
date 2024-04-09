8f82dced0e277a9b6069aa8db869609f60a078d9
Ethan Groene <ethangroene@gmail.com>
2024-04-08 22:35:48 -0600

    Start tracking Error404 comp

---
+++ src/Components/Error404/Error404.tsx
@@ -0,0 +1,12 @@
+const Error404 = () => {
+  return (
+    <>
+      <h1>Hmmm, looks like that page doesn't exist...</h1>
+      <p>
+        Double-check the URL to make sure it's correct. Otherwise, we're not sure what to
+        tell you.
+      </p>
+    </>
+  );
+};
+export default Error404;
