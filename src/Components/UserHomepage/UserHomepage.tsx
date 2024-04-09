26e4b386286815e578ba6a2e22902d1d11fe5ddf
Ethan Groene <ethangroene@gmail.com>
2024-04-08 22:50:03 -0600

    Update BrowserRouter after UserHomepage renders
    ...depending on 'navigation' & 'currentUser' variables

--- src/Components/UserHomepage/UserHomepage.tsx
+++ src/Components/UserHomepage/UserHomepage.tsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { useMainContext } from "../../Hooks/useMainContext";
+import { useEffect } from "react";
 
 const UserHomepage = () => {
   const { currentUser } = useMainContext();
 
   /* If currentUser is undefined, redirect to base URL (/). This prevents access to user account by simply pasting in their acct url. Forces login. Also, this component will only render if currentUser exists */
   const navigation = useNavigate();
-  if (!currentUser) {
-    navigation("/");
-  }
+  useEffect(() => {
+    if (!currentUser) {
+      navigation("/");
+    }
+  }, [currentUser, navigation]);
 
   return (
     currentUser && (
