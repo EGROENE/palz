41ea081615fa5b2276411b7e934ea268929e58c4
Ethan Groene <ethangroene@gmail.com>
2024-04-12 11:27:49 -0600

    Build out Sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,15 +1,51 @@
+import { useState, useEffect } from "react";
 import { useMainContext } from "../../Hooks/useMainContext";
+import { useUserContext } from "../../Hooks/useUserContext";
 
 const Sidebar = () => {
-  const { currentUser } = useMainContext();
+  const { currentUser, theme } = useMainContext();
+  const { showSidebar, setShowSidebar } = useUserContext();
+  const [borderColor, setBorderColor] = useState<string>("");
+
+  useEffect(() => {
+    const themeColors = [
+      "var(--theme-blue)",
+      "var(--theme-green)",
+      "var(--theme-red)",
+      "var(--theme-purple)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setBorderColor(themeColors[randomNumber]);
+  }, []);
 
   return (
-    <div className="sidebar">
+    <div
+      className="sidebar"
+      style={
+        theme === "dark"
+          ? {
+              border: `2px solid ${borderColor}`,
+              backgroundColor: "black",
+            }
+          : { border: `2px solid ${borderColor}`, backgroundColor: "white" }
+      }
+    >
+      <i
+        onClick={() => setShowSidebar(!showSidebar)}
+        style={{ backgroundColor: borderColor }}
+        className="fas fa-times"
+      ></i>
       <img className="profile-image-sidebar" src={currentUser?.profileImage} />
-      <div>
+      <div style={{ backgroundColor: borderColor }} className="sidebar-names-container">
         <p>{`${currentUser?.firstName} ${currentUser?.lastName}`}</p>
         <p>{currentUser?.username}</p>
       </div>
+      <p className="sidebar-options">Create Event</p>
+      <p className="sidebar-options">Find Palz</p>
+      <p className="sidebar-options">Explore Events</p>
+      <p className="sidebar-options">My Palz</p>
+      <p className="sidebar-options">Find Events</p>
+      <p className="sidebar-options">Settings</p>
     </div>
   );
 };
