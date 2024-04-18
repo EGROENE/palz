bd28fef8a02ea26d1d2400044b260cec76a14a08
Ethan Groene <ethangroene@gmail.com>
2024-04-18 15:06:08 -0600

    Add route to user settings page
    Also, ensure redirection to user homepage once logged in & current url is their homepage. This prevents them from navving back to login w/ browser nav arrows.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import LoginPage from "./Components/LoginPage/LoginPage";
 import Welcome from "./Components/Welcome/Welcome";
 import UserHomepage from "./Components/UserHomepage/UserHomepage";
 import { useMainContext } from "./Hooks/useMainContext";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import Error404 from "./Components/Error404/Error404";
+import UserSettings from "./Components/UserSettings/UserSettings";
 
 function App() {
   const { userCreatedAccount, theme, showWelcomeMessage, currentUser } = useMainContext();
 
   const navigation = useNavigate();
+  const currentURL = useLocation().pathname;
 
   theme === "dark"
     ? (document.body.style.backgroundColor = "#242424")
@@ -19,10 +21,20 @@ function App() {
   /* Redirect to user homepage if user has logged in or created account, welcome message not shown (user not able to nav back to login once logged in) */
   /* userCreatedAccount is checked for non-null values, since currentUser may be set before submission of login form, causing user's homepage to display before they actually log in */
   useEffect(() => {
-    if (userCreatedAccount !== null && !showWelcomeMessage) {
+    if (
+      userCreatedAccount !== null &&
+      !showWelcomeMessage &&
+      currentURL === `/users/${currentUser?.username}`
+    ) {
       navigation(`users/${currentUser?.username}`);
     }
-  }, [navigation, showWelcomeMessage, userCreatedAccount, currentUser?.username]);
+  }, [
+    navigation,
+    showWelcomeMessage,
+    userCreatedAccount,
+    currentUser?.username,
+    currentURL,
+  ]);
 
   const getBaseURLElement = (): JSX.Element => {
     if (userCreatedAccount !== null && showWelcomeMessage) {
@@ -38,6 +50,7 @@ function App() {
     <div className="app" data-theme={theme}>
       <Routes>
         <Route path="/" element={baseURLElement} />
+        <Route path="/settings" element={<UserSettings />} />
         <Route path="/users/:username" element={<UserHomepage />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
