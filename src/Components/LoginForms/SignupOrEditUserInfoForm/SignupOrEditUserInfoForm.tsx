765a5109c3f872b7c02e86055a911715d10b035c
Ethan Groene <ethangroene@gmail.com>
2024-04-26 10:52:38 -0600

    Add header over edit-user-info form

--- src/Components/LoginForms/SignupOrEditUserInfoForm/SignupOrEditUserInfoForm.tsx
+++ src/Components/LoginForms/SignupOrEditUserInfoForm/SignupOrEditUserInfoForm.tsx
@@ -294,148 +294,133 @@ const SignupOrEditUserInfoForm = ({ isOnSignup }: { isOnSignup: boolean }) => {
   const confirmPasswordFieldClass = getConfirmPasswordFieldClass();
 
   return (
-    <form
-      onSubmit={(e) => {
-        isOnSignup ? handleSignupFormSubmission(e) : handleUpdateProfileInfo(e);
-      }}
-      className="login-signup-form"
-    >
-      <div>
-        <label>
-          <p>First Name:</p>
-          <input
-            onChange={(e) =>
-              isOnSignup
-                ? handleNameInput(e.target.value, true, true)
-                : handleNameInput(e.target.value, true, false)
-            }
-            value={firstName}
-            type="text"
-            placeholder={isOnSignup ? "Enter your first name" : "Change first name"}
-            inputMode="text"
-            className={firstNameFieldClass}
-          />
-          {showFirstNameError && <p className="input-error-message">{firstNameError}</p>}
-        </label>
-        <label>
-          <p>Last Name:</p>
-          <input
-            onChange={(e) =>
-              isOnSignup
-                ? handleNameInput(e.target.value, false, true)
-                : handleNameInput(e.target.value, false, false)
-            }
-            type="text"
-            value={lastName}
-            placeholder={isOnSignup ? "Enter your last name" : "Change last name"}
-            inputMode="text"
-            className={lastNameFieldClass}
-          />
-          {showLastNameError && <p className="input-error-message">{lastNameError}</p>}
-        </label>
-      </div>
-      <label>
-        <p>
-          Username:{" "}
-          <span>
-            <i
-              onClick={() => setShowUsernameCriteria(!showUsernameCriteria)}
-              className="fas fa-info-circle"
-              title="Must be 4-20 characters long & contain only alphanumeric characters"
-            ></i>
-          </span>
-        </p>
-        {showUsernameCriteria && (
-          <p className="input-criteria">
-            Must be 4-20 characters long & contain only alphanumeric characters
-          </p>
-        )}
-        <input
-          title="Must be 4-20 characters long & contain only alphanumeric characters"
-          onChange={(e) =>
-            isOnSignup
-              ? handleUsernameInput(e.target.value, true)
-              : handleUsernameInput(e.target.value, false)
-          }
-          value={username}
-          type="text"
-          placeholder="Enter a username"
-          inputMode="text"
-          className={usernameFieldClass}
-        />
-        {showUsernameError && <p className="input-error-message">{usernameError}</p>}
-      </label>
-      <label>
-        <p>E-Mail Address:</p>
-        <input
-          onChange={(e) =>
-            isOnSignup
-              ? handleEmailAddressInput(e.target.value, true)
-              : handleEmailAddressInput(e.target.value, false)
-          }
-          value={emailAddress}
-          type="email"
-          placeholder="Enter your e-mail address"
-          inputMode="email"
-          className={emailFieldClass}
-        />
-        {showEmailError && <p className="input-error-message">{emailError}</p>}
-      </label>
-      <label>
-        <p>
-          {isOnSignup ? "Choose a Password: " : "Password: "}
-          <span>
-            <i
-              onClick={() => setShowPasswordCriteria(!showPasswordCriteria)}
-              className="fas fa-info-circle"
-              title="Must contain at least one uppercase & one lowercase English letter, at least one digit, at least one special character, & be 8-50 characters long. No spaces allowed."
-            ></i>
-          </span>
-        </p>
-        {showPasswordCriteria && (
-          <p className="input-criteria">
-            Must contain at least one uppercase & one lowercase English letter, at least
-            one digit, at least one special character, & be 8-20 characters long. No
-            spaces allowed.
-          </p>
-        )}
-
-        <div className="password-input">
-          <input
-            onChange={(e) =>
-              isOnSignup
-                ? handlePasswordInput(e.target.value, "signup")
-                : handlePasswordInput(e.target.value, "edit-user-info")
-            }
-            value={password}
-            type={passwordIsHidden ? "password" : "text"}
-            placeholder={isOnSignup === false ? "Enter new password" : "Enter password"}
-            inputMode="text"
-            className={passwordFieldClass}
-          />
-          {!passwordIsHidden ? (
-            <OpenEye toggleHidePassword={toggleHidePassword} />
-          ) : (
-            <ClosedEye toggleHidePassword={toggleHidePassword} />
-          )}
+    <>
+      {!isOnSignup && (
+        <h2>
+          Feel free to change any of your profile info in the form below. Make sure to
+          save changes, else they won't apply
+        </h2>
+      )}
+      <form
+        onSubmit={(e) => {
+          isOnSignup ? handleSignupFormSubmission(e) : handleUpdateProfileInfo(e);
+        }}
+        className="login-signup-form"
+      >
+        <div>
+          <label>
+            <p>First Name:</p>
+            <input
+              onChange={(e) =>
+                isOnSignup
+                  ? handleNameInput(e.target.value, true, true)
+                  : handleNameInput(e.target.value, true, false)
+              }
+              value={firstName}
+              type="text"
+              placeholder={isOnSignup ? "Enter your first name" : "Change first name"}
+              inputMode="text"
+              className={firstNameFieldClass}
+            />
+            {showFirstNameError && (
+              <p className="input-error-message">{firstNameError}</p>
+            )}
+          </label>
+          <label>
+            <p>Last Name:</p>
+            <input
+              onChange={(e) =>
+                isOnSignup
+                  ? handleNameInput(e.target.value, false, true)
+                  : handleNameInput(e.target.value, false, false)
+              }
+              type="text"
+              value={lastName}
+              placeholder={isOnSignup ? "Enter your last name" : "Change last name"}
+              inputMode="text"
+              className={lastNameFieldClass}
+            />
+            {showLastNameError && <p className="input-error-message">{lastNameError}</p>}
+          </label>
         </div>
-        {showPasswordError && <p className="input-error-message">{passwordError}</p>}
-      </label>
-      {/* Render 'confirm pw' field only if form is on signup or if it's form to edit user info, & pw has been changed */}
-      {((isOnSignup === false && password !== currentUser?.password && password !== "") ||
-        isOnSignup === true) && (
         <label>
-          <p>{isOnSignup ? "Confirm Password:" : "Confirm New Password:"}</p>
+          <p>
+            Username:{" "}
+            <span>
+              <i
+                onClick={() => setShowUsernameCriteria(!showUsernameCriteria)}
+                className="fas fa-info-circle"
+                title="Must be 4-20 characters long & contain only alphanumeric characters"
+              ></i>
+            </span>
+          </p>
+          {showUsernameCriteria && (
+            <p className="input-criteria">
+              Must be 4-20 characters long & contain only alphanumeric characters
+            </p>
+          )}
+          <input
+            title="Must be 4-20 characters long & contain only alphanumeric characters"
+            onChange={(e) =>
+              isOnSignup
+                ? handleUsernameInput(e.target.value, true)
+                : handleUsernameInput(e.target.value, false)
+            }
+            value={username}
+            type="text"
+            placeholder="Enter a username"
+            inputMode="text"
+            className={usernameFieldClass}
+          />
+          {showUsernameError && <p className="input-error-message">{usernameError}</p>}
+        </label>
+        <label>
+          <p>E-Mail Address:</p>
+          <input
+            onChange={(e) =>
+              isOnSignup
+                ? handleEmailAddressInput(e.target.value, true)
+                : handleEmailAddressInput(e.target.value, false)
+            }
+            value={emailAddress}
+            type="email"
+            placeholder="Enter your e-mail address"
+            inputMode="email"
+            className={emailFieldClass}
+          />
+          {showEmailError && <p className="input-error-message">{emailError}</p>}
+        </label>
+        <label>
+          <p>
+            {isOnSignup ? "Choose a Password: " : "Password: "}
+            <span>
+              <i
+                onClick={() => setShowPasswordCriteria(!showPasswordCriteria)}
+                className="fas fa-info-circle"
+                title="Must contain at least one uppercase & one lowercase English letter, at least one digit, at least one special character, & be 8-50 characters long. No spaces allowed."
+              ></i>
+            </span>
+          </p>
+          {showPasswordCriteria && (
+            <p className="input-criteria">
+              Must contain at least one uppercase & one lowercase English letter, at least
+              one digit, at least one special character, & be 8-20 characters long. No
+              spaces allowed.
+            </p>
+          )}
+
           <div className="password-input">
             <input
               onChange={(e) =>
-                handleConfirmationPasswordInput(e.target.value, isOnSignup)
+                isOnSignup
+                  ? handlePasswordInput(e.target.value, "signup")
+                  : handlePasswordInput(e.target.value, "edit-user-info")
               }
-              value={confirmationPassword}
+              value={password}
               type={passwordIsHidden ? "password" : "text"}
-              placeholder="Confirm password"
+              placeholder={isOnSignup === false ? "Enter new password" : "Enter password"}
               inputMode="text"
-              className={confirmPasswordFieldClass}
+              className={passwordFieldClass}
             />
             {!passwordIsHidden ? (
               <OpenEye toggleHidePassword={toggleHidePassword} />
@@ -443,43 +428,70 @@ const SignupOrEditUserInfoForm = ({ isOnSignup }: { isOnSignup: boolean }) => {
               <ClosedEye toggleHidePassword={toggleHidePassword} />
             )}
           </div>
-          {confirmationPasswordError !== "" &&
-            confirmationPasswordError !== "Please fill out this field" && (
+          {showPasswordError && <p className="input-error-message">{passwordError}</p>}
+        </label>
+        {/* Render 'confirm pw' field only if form is on signup or if it's form to edit user info, & pw has been changed */}
+        {((isOnSignup === false &&
+          password !== currentUser?.password &&
+          password !== "") ||
+          isOnSignup === true) && (
+          <label>
+            <p>{isOnSignup ? "Confirm Password:" : "Confirm New Password:"}</p>
+            <div className="password-input">
+              <input
+                onChange={(e) =>
+                  handleConfirmationPasswordInput(e.target.value, isOnSignup)
+                }
+                value={confirmationPassword}
+                type={passwordIsHidden ? "password" : "text"}
+                placeholder="Confirm password"
+                inputMode="text"
+                className={confirmPasswordFieldClass}
+              />
+              {!passwordIsHidden ? (
+                <OpenEye toggleHidePassword={toggleHidePassword} />
+              ) : (
+                <ClosedEye toggleHidePassword={toggleHidePassword} />
+              )}
+            </div>
+            {confirmationPasswordError !== "" &&
+              confirmationPasswordError !== "Please fill out this field" && (
+                <p className="input-error-message">{confirmationPasswordError}</p>
+              )}
+            {confirmationPasswordError === "Please fill out this field" && showErrors && (
               <p className="input-error-message">{confirmationPasswordError}</p>
             )}
-          {confirmationPasswordError === "Please fill out this field" && showErrors && (
-            <p className="input-error-message">{confirmationPasswordError}</p>
-          )}
-        </label>
-      )}
-      {isOnSignup ? (
-        <button
-          className="login-button"
-          type={areNoSignupOrEditFormErrors ? "submit" : "button"}
-          onClick={(e) =>
-            areNoSignupOrEditFormErrors && allSignupOrEditFormFieldsFilled
-              ? undefined
-              : handleFormRejection(e)
-          }
-        >
-          Sign Up
-        </button>
-      ) : (
-        <div className="edit-user-info-form-options">
-          <button disabled={!userInfoEdited} onClick={() => handleEditUserInfoRevert()}>
-            Revert Changes
-          </button>
+          </label>
+        )}
+        {isOnSignup ? (
           <button
-            type="submit"
-            disabled={!userInfoEdited}
-            style={{ backgroundColor: randomColor }}
-            onClick={(e) => handleUpdateProfileInfo(e)}
+            className="login-button"
+            type={areNoSignupOrEditFormErrors ? "submit" : "button"}
+            onClick={(e) =>
+              areNoSignupOrEditFormErrors && allSignupOrEditFormFieldsFilled
+                ? undefined
+                : handleFormRejection(e)
+            }
           >
-            Save Changes
+            Sign Up
           </button>
-        </div>
-      )}
-    </form>
+        ) : (
+          <div className="edit-user-info-form-options">
+            <button disabled={!userInfoEdited} onClick={() => handleEditUserInfoRevert()}>
+              Revert Changes
+            </button>
+            <button
+              type="submit"
+              disabled={!userInfoEdited}
+              style={{ backgroundColor: randomColor }}
+              onClick={(e) => handleUpdateProfileInfo(e)}
+            >
+              Save Changes
+            </button>
+          </div>
+        )}
+      </form>
+    </>
   );
 };
 export default SignupOrEditUserInfoForm;
