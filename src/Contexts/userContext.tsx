c6977bcfc43c2c53a9ae4cf651a7d63fb946bbe1
Ethan Groene <ethangroene@gmail.com>
2024-04-29 22:45:01 -0600

    Def logout method
    This functionality is used in multiple comps. Any additional functionality can be put along w/ this inside of a handler function in child components.

--- src/Contexts/userContext.tsx
+++ src/Contexts/userContext.tsx
@@ -1,14 +1,26 @@
 import { createContext, ReactNode, useState } from "react";
 import { TUserContext } from "../types";
+import { useMainContext } from "../Hooks/useMainContext";
+import { useLoginContext } from "../Hooks/useLoginContext";
 
 export const UserContext = createContext<TUserContext | null>(null);
 
 export const UserContextProvider = ({ children }: { children: ReactNode }) => {
+  const { removeCurrentUser, setUserCreatedAccount } = useMainContext();
+  const { resetFormFieldsAndErrors } = useLoginContext();
+
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
+
+  const logout = (): void => {
+    setUserCreatedAccount(null);
+    removeCurrentUser();
+    resetFormFieldsAndErrors();
+  };
 
   const userContextValues: TUserContext = {
     showSidebar,
     setShowSidebar,
+    logout,
   };
 
   return (
