3e43044b64061fbf0c06fd99fdbfcc692548d22e
Ethan Groene <ethangroene@gmail.com>
2024-04-29 22:48:32 -0600

    Add func for user to del acct

--- src/Components/UserSettings/UserSettings.tsx
+++ src/Components/UserSettings/UserSettings.tsx
@@ -6,11 +6,13 @@ import { useUserContext } from "../../Hooks/useUserContext";
 import SignupOrEditUserInfoForm from "../LoginForms/SignupOrEditUserInfoForm/SignupOrEditUserInfoForm";
 import NavBar from "../NavBar/NavBar";
 import Sidebar from "../Sidebar/Sidebar";
+import Requests from "../../requests";
+import toast from "react-hot-toast";
 
 const UserSettings = () => {
   const { currentUser } = useMainContext();
   const { passwordIsHidden, setPasswordIsHidden } = useLoginContext();
-  const { showSidebar, setShowSidebar } = useUserContext();
+  const { showSidebar, setShowSidebar, logout } = useUserContext();
 
   useEffect(() => {
     if (showSidebar) {
@@ -30,12 +32,31 @@ const UserSettings = () => {
     }
   }, []);
 
+  const handleAccountDeletion = () => {
+    Requests.deleteUser(currentUser?.id)
+      .then((response) => {
+        if (!response.ok) {
+          toast.error("Could not delete your account. Please try again.");
+        } else {
+          toast.error("You have deleted your account. We're sorry to see you go!");
+          logout();
+          navigation("/");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <>
       <NavBar />
       {showSidebar && <Sidebar />}
       <h1>Settings</h1>
       <SignupOrEditUserInfoForm isOnSignup={false} />
+      <h3>Delete Account</h3>
+      <p>Please understand that this action is irreversible</p>
+      <button onClick={() => handleAccountDeletion()} className="delete-button">
+        Delete Account
+      </button>
     </>
   );
 };
