dcda6ef87f1437fb659da66b75f30d297e1d4cf8
Ethan Groene <ethangroene@gmail.com>
2024-06-01 16:07:03 -0400

    Add EventsPage component
    Contains universal navbar, header, & EventCard for every event so far. So far, on every EventCard, user can add/delete RSVP to given event.

---
+++ src/Components/EventsPage/EventsPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useMainContext } from "../../Hooks/useMainContext";
+import { useUserContext } from "../../Hooks/useUserContext";
+import NavBar from "../NavBar/NavBar";
+import EventCard from "../EventCard/EventCard";
+
+const EventsPage = () => {
+  const { fetchAllEvents, allEvents } = useMainContext();
+  const { showSidebar, setShowSidebar } = useUserContext();
+
+  useEffect(() => {
+    fetchAllEvents();
+  }, []);
+
+  return (
+    <div
+      style={{ "width": "unset" }}
+      className="events-page-hero"
+      onClick={() => showSidebar && setShowSidebar(false)}
+    >
+      <NavBar />
+      <h1>Events</h1>
+      <div className="all-events-container">
+        {allEvents.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventsPage;
