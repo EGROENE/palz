1b9c8f48b3f0235a5e9cf8a97a727ca4edb1105d
Ethan Groene <ethangroene@gmail.com>
2024-06-03 23:22:11 -0400

    Add EventsPage w/ ImageSlideshow & basic styling

---
+++ src/Components/EventPage/EventPage.tsx
@@ -0,0 +1,127 @@
+import { TEvent } from "../../types";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useMainContext } from "../../Hooks/useMainContext";
+import { useUserContext } from "../../Hooks/useUserContext";
+import ImageSlideshow from "../ImageSlideshow/ImageSlideshow";
+import NavBar from "../NavBar/NavBar";
+
+const EventPage = () => {
+  const { allEvents, getMostCurrentEvents, currentUser } = useMainContext();
+  const { handleDeleteUserRSVP, handleAddUserRSVP, showSidebar, setShowSidebar } =
+    useUserContext();
+  const { eventID } = useParams();
+  const [event, setEvent] = useState<TEvent>(
+    allEvents.filter((ev) => ev.id === eventID)[0]
+  );
+
+  const [randomColor, setRandomColor] = useState<string>("");
+  useEffect(() => {
+    getMostCurrentEvents();
+    const themeColors = [
+      "var(--theme-blue)",
+      "var(--theme-green)",
+      "var(--theme-red)",
+      "var(--theme-purple)",
+      "var(--theme-orange)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+  }, []);
+
+  useEffect(() => {
+    setEvent(allEvents.filter((ev) => ev.id === eventID)[0]);
+  }, [allEvents]);
+
+  const nextEventDateTime = event ? new Date(event.nextEventTime) : undefined;
+
+  const userRSVPd =
+    currentUser?.username && event?.interestedUsers.includes(currentUser.username);
+
+  const getImagesArray = ():
+    | {
+        url: string | undefined;
+        altText: string | undefined;
+      }[]
+    | undefined => {
+    if (
+      event?.imageOne?.src !== "" &&
+      event?.imageTwo?.src !== "" &&
+      event?.imageThree?.src !== ""
+    ) {
+      return [
+        { url: event?.imageOne?.src, altText: event?.imageOne?.altText },
+        { url: event?.imageTwo?.src, altText: event?.imageTwo?.altText },
+        { url: event?.imageThree?.src, altText: event?.imageThree?.altText },
+      ];
+    } else if (event?.imageOne?.src !== "" && event?.imageTwo?.src !== "") {
+      return [
+        { url: event?.imageOne?.src, altText: event?.imageOne?.altText },
+        { url: event?.imageTwo?.src, altText: event?.imageTwo?.altText },
+      ];
+    } else if (event?.imageOne?.src !== "") {
+      return [{ url: event?.imageOne?.src, altText: event?.imageOne?.altText }];
+    }
+  };
+  const eventImages = getImagesArray();
+
+  return (
+    <div onClick={() => showSidebar && setShowSidebar(false)} className="event-page-hero">
+      <NavBar />
+      {event ? (
+        <>
+          <div
+            style={{ "border": `2px solid ${randomColor}` }}
+            className="event-main-info-container"
+          >
+            <h1 style={{ "color": randomColor }}>{event.title}</h1>
+            <div className="event-main-info-text-container">
+              <div>
+                <p>
+                  {nextEventDateTime?.toDateString()} at{" "}
+                  {nextEventDateTime?.toLocaleTimeString()}
+                </p>
+                <p>{`${event.address} ${event.postalCode}`}</p>
+                <p>{`${event.city}, ${event.stateProvince}, ${event.country}`}</p>
+              </div>
+              <div>
+                {event?.organizers.length === 1 ? (
+                  <p>Organizer: {event.organizers[0]}</p>
+                ) : (
+                  <p>Organizers: {event.organizers.join(", ")}</p>
+                )}
+                <p>{`RSVPs: ${event.interestedUsers.length}`}</p>
+              </div>
+            </div>
+            <button
+              style={{ "backgroundColor": randomColor }}
+              onClick={(e) =>
+                userRSVPd ? handleDeleteUserRSVP(e, event) : handleAddUserRSVP(e, event)
+              }
+            >
+              {userRSVPd ? "Remove RSVP" : "RSVP"}
+            </button>
+          </div>
+          <div className="further-event-info-container">
+            {event.imageOne?.src !== "" && (
+              <ImageSlideshow randomColor={randomColor} images={eventImages} />
+            )}
+            <div>
+              <p>{event?.description}</p>
+              {event?.additionalInfo !== "" && <p>{event?.additionalInfo}</p>}
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>Sorry, this event doesn't exist anymore.</h1>
+          <Link to={"/events"}>
+            <button>Back to All Events</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default EventPage;
---
+++ src/Components/ImageSlideshow/ImageSlideshow.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+type TDirection = "next" | "prev";
+
+const ImageSlideshow = ({
+  images,
+  randomColor,
+}: {
+  images:
+    | {
+        url: string | undefined;
+        altText: string | undefined;
+      }[]
+    | undefined;
+  randomColor: string;
+}) => {
+  const [imgIndex, setImgIndex] = useState<number>(0);
+
+  const [slideshowDirection, setSlideshowDirection] = useState<TDirection>("next");
+
+  const changeImage = (direction: TDirection): void => {
+    /* Necessary to setSlideshowDirection state value here (its existence is necessary, too) because, if an image doesn't exist (error 404 occurs on the image) while user is scrolling through a park's images, the image that follows the erroneous image in the sequence, whether the user is moving forward or backward through the image array, should be displayed, & the onError callback function needs to know the direction in which the user is scrolling through the images. */
+    if (direction !== slideshowDirection) {
+      setSlideshowDirection(direction);
+    }
+    if (imgIndex !== undefined) {
+      if (direction === "next" && images) {
+        imgIndex === images.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1);
+      } else {
+        imgIndex === 0 && images
+          ? setImgIndex(images.length - 1)
+          : setImgIndex(imgIndex - 1);
+      }
+    }
+  };
+
+  return (
+    <div className="slideshow-container">
+      <div
+        className={
+          images && images.length > 1
+            ? "img-arrows-container"
+            : "img-arrows-container centered"
+        }
+      >
+        {images && images.length > 1 && (
+          <i
+            onClick={() => {
+              changeImage("prev");
+            }}
+            className="fas fa-angle-right"
+            title="Previous Image"
+          ></i>
+        )}
+        <div className="slideshow-img-container">
+          {/* If imgIndex is specifically not undefined (not just falsy), show image/alt text. imgIndex should never be falsy, as its value is set whenever this component renders, though it is initialized as undefined. It's not good practice to initialize it, then change it again as soon as component renders. */}
+          {imgIndex !== undefined && (
+            <img
+              src={images && images[imgIndex].url}
+              alt={images && images[imgIndex].altText}
+              onError={() => changeImage(slideshowDirection)}
+              style={{ "border": `2px solid ${randomColor}` }}
+            />
+          )}
+        </div>
+        {images && images.length > 1 && (
+          <i
+            onClick={() => {
+              changeImage("next");
+            }}
+            className="fas fa-angle-right"
+            title="Next Image"
+          ></i>
+        )}
+      </div>
+    </div>
+  );
+};
+export default ImageSlideshow;
