d375ad27a7188edf135f8a5d02afbfcb3b5b8a6b
Ethan Groene <ethangroene@gmail.com>
2024-06-04 16:35:12 -0400

    Ensure Error404 comp message is centered

--- src/Components/Error404/Error404.tsx
+++ src/Components/Error404/Error404.tsx
@@ -1,12 +1,12 @@
 const Error404 = () => {
   return (
-    <>
+    <div style={{ "width": " 100vw", "margin": "0" }} className="page-hero">
       <h1>Hmmm, looks like that page doesn't exist...</h1>
       <p>
         Double-check the URL to make sure it's correct. Otherwise, we're not sure what to
         tell you.
       </p>
-    </>
+    </div>
   );
 };
 export default Error404;
