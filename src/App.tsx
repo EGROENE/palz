e4dd0aeed95e1bcba9f94f6ca46e2951d8c3caaa
Ethan Groene <ethangroene@gmail.com>
2024-06-04 22:50:15 -0400

    Set up route to EventForm

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { useUserContext } from "./Hooks/useUserContext";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import Error404 from "./Components/Error404/Error404";
 import UserSettings from "./Components/UserSettings/UserSettings";
+import EventForm from "./Components/EventForm/EventForm";
 import EventsPage from "./Components/EventsPage/EventsPage";
 import EventPage from "./Components/EventPage/EventPage";
 
@@ -57,6 +58,7 @@ function App() {
       <Routes>
         <Route path="/" element={baseURLElement} />
         <Route path="/settings" element={<UserSettings />} />
+        <Route path="/event-form" element={<EventForm />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/events/:eventID" element={<EventPage />} />
         <Route path="/users/:username" element={<UserHomepage />} />
