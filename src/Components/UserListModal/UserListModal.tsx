468d3bb6881983798ff6e384271d963bc6cba76c
Ethan Groene <ethangroene@gmail.com>
2024-06-18 18:33:00 -0400

    Accept header prop to display above user list

--- src/Components/UserListModal/UserListModal.tsx
+++ src/Components/UserListModal/UserListModal.tsx
@@ -3,14 +3,17 @@ import { TUser, TEvent } from "../../types";
 /* Component contains a modal w/ background, as well as a list of users. In every user box, there is user image, name, username, & button that will eventually make it possible to message user & a button that removes user from list. To be used on event pages to show list of RSVPs & list of invitees. */
 const UserListModal = ({
   closeModalMethod,
+  header,
   handleUserRemoval,
   userArray,
   event,
   randomColor,
 }: {
   closeModalMethod: (value: React.SetStateAction<boolean>) => void;
+  header: string;
   handleUserRemoval: (
     e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+
     event: TEvent,
     user: TUser
   ) => void;
@@ -26,7 +29,7 @@ const UserListModal = ({
         className="fas fa-times close-module-icon"
       ></i>
       <div className="user-list-container">
-        <h2>Invitees</h2>
+        <h2>{header}</h2>
         {userArray.map((user) => (
           <div key={user.id} className="listed-user">
             <img
