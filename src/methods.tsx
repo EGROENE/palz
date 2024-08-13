0c973bcf21cde399f7558774e095ef8e4ca2ab3d
Ethan Groene <ethangroene@gmail.com>
2024-08-12 21:41:49 -0400

    Add method to check if 2 arrs have same values

--- src/methods.tsx
+++ src/methods.tsx
@@ -1,5 +1,23 @@
 import { TEvent } from "./types";
 
+const arraysAreIdentical = (_arr1: any[], _arr2: any[]) => {
+  if (!Array.isArray(_arr1) || !Array.isArray(_arr2) || _arr1.length !== _arr2.length) {
+    return false;
+  }
+
+  // .concat() to not mutate arguments
+  const arr1 = _arr1.concat().sort();
+  const arr2 = _arr2.concat().sort();
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const sortEventsSoonestToLatest = (eventArray: TEvent[]): TEvent[] =>
   eventArray.sort((a, b) => a.nextEventTime - b.nextEventTime);
 
@@ -125,6 +143,7 @@ const getStringArraySortedAlphabetically = (array: string[]): string[] => {
 };
 
 const Methods = {
+  arraysAreIdentical,
   sortEventsSoonestToLatest,
   isValidUrl,
   nameNoSpecialChars,
