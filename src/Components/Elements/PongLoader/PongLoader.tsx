aa85a652873b5b83c6bf07ec292e5913b7bc5163
Ethan Groene <ethangroene@gmail.com>
2024-08-19 23:06:05 -0400

    Add PongLoader

---
+++ src/Components/Elements/PongLoader/PongLoader.tsx
@@ -0,0 +1,13 @@
+const PongLoader = () => {
+  return (
+    /* From Uiverse.io by TemRevil */
+    <div className="loading">
+      <div className="loading-box">
+        <div className="WH color l1"></div>
+        <div className="ball color"></div>
+        <div className="WH color l2"></div>
+      </div>
+    </div>
+  );
+};
+export default PongLoader;
