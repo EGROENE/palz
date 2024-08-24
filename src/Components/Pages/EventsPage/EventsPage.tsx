8abf46e3051c950aed77c98360c210367a8cc5ef
Ethan Groene <ethangroene@gmail.com>
2024-08-24 16:54:38 -0400

    Add filter conditions for displayedEvents
    Only events whose end is in future, or start is in future, or whose end is unset & start is now or in future, or whose end is unset & start is  at mos 1hr in pastfro now, are displayed

--- src/Components/Pages/EventsPage/EventsPage.tsx
+++ src/Components/Pages/EventsPage/EventsPage.tsx
@@ -4,6 +4,7 @@ import { useUserContext } from "../../../Hooks/useUserContext";
 import { useNavigate } from "react-router-dom";
 import EventCard from "../../Elements/EventCard/EventCard";
 import Methods from "../../../methods";
+import { TEvent } from "../../../types";
 
 const EventsPage = () => {
   const { allEvents, fetchAllEvents, currentUser, userCreatedAccount } = useMainContext();
@@ -23,11 +24,28 @@ const EventsPage = () => {
     }
   }, []);
 
+  const now = Date.now();
+  const getDisplayedEvents = (): TEvent[] => {
+    // Later, add filter conditionals here...
+    // 'happening now' events should be only events whose end is not -1 (unset) & whose start is before now & end is after
+
+    // If no filters, return events whose start is before now & end is after, or whose end is -1 (unset) and start time is within an hour of now, plus events whose start is in future
+    return allEvents.filter(
+      (event) =>
+        event.eventEndDateTimeInMS > now || // end is in future
+        event.eventStartDateTimeInMS > now || // start is in future
+        (event.eventEndDateTimeInMS === -1 && event.eventStartDateTimeInMS >= now) || // end unset & start is now or in future
+        (event.eventEndDateTimeInMS === -1 &&
+          event.eventStartDateTimeInMS <= now - 3600000) // end unset & start is at most 1hr in past from now
+    );
+  };
+  const displayedEvents: TEvent[] = getDisplayedEvents();
+
   return (
     <div className="page-hero" onClick={() => showSidebar && setShowSidebar(false)}>
       <h1>Events</h1>
       <div className="all-events-container">
-        {Methods.sortEventsSoonestToLatest(allEvents).map((event) => (
+        {Methods.sortEventsSoonestToLatest(displayedEvents).map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
       </div>
