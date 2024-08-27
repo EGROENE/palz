55f48ae2f0a637506e72d048ad2062efb141b056
Ethan Groene <ethangroene@gmail.com>
2024-08-27 12:24:38 -0400

    Implement SearchBar

--- src/Components/Elements/InterestsModal/InterestsModal.tsx
+++ src/Components/Elements/InterestsModal/InterestsModal.tsx
@@ -1,4 +1,6 @@
 import Methods from "../../../methods";
+import { TThemeColor } from "../../../types";
+import SearchBar from "../SearchBar/SearchBar";
 import Tab from "../Tab/Tab";
 
 /* Returns a modal wherein user can see a bunch of tabs, each of which represent an interest & a '+', which adds interest to certain array. There is also a search bar that can be used to find interests or type in a nonexistent interest to be added. */
@@ -29,7 +31,7 @@ const InterestsModal = ({
   noAdditionalInterestsAndInputInterest: boolean;
   noAdditionalInterestsAndNoInputInterest: boolean;
   disableAddInterestsButton: boolean;
-  randomColor?: string;
+  randomColor?: TThemeColor;
   interestsRelation: "event" | "user";
 }) => {
   return (
@@ -45,31 +47,17 @@ const InterestsModal = ({
       <div className="browse-interests-modal">
         <div className="bar-and-description">
           <p>Don't see an interest listed? Type it below & add it:</p>
-          <div className="add-interests-bar">
-            <input
-              value={inputInterest}
-              onChange={(e) => inputInterestsHandler(e.target.value)}
-              type="text"
-              placeholder="Type an interest"
-            ></input>
-            {inputInterest !== "" && (
-              <i
-                title="Clear"
-                onClick={() => handleClearAddInterestInput()}
-                className="fas fa-times"
-              ></i>
-            )}
-            <button
-              onClick={(e) => {
-                handleAddInterest(inputInterest, e);
-                handleClearAddInterestInput();
-              }}
-              disabled={disableAddInterestsButton}
-              style={{ backgroundColor: randomColor }}
-            >
-              Add
-            </button>
-          </div>
+          <SearchBar
+            input={inputInterest}
+            placeholder="Type an interest"
+            inputHandler={inputInterestsHandler}
+            clearInputHandler={handleClearAddInterestInput}
+            isSideButton={true}
+            sideButtonText="Add"
+            sideButtonIsDisabled={disableAddInterestsButton}
+            addMethod={handleAddInterest}
+            randomColor={randomColor}
+          />
         </div>
         <div className="non-user-interests-container">
           {!noAdditionalInterestsAndNoInputInterest &&
