c13ab97d57f09b19b7c28fa1865cd794a7e23179
Ethan Groene <ethangroene@gmail.com>
2024-08-28 19:48:03 -0400

    Build out appearance of SearchBar
    Styling, title; also, only call addMethod in onClick of side btn if it has been passed to component

--- src/Components/Elements/SearchBar/SearchBar.tsx
+++ src/Components/Elements/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { TThemeColor } from "../../../types";
+import styles from "./styles.module.css";
 
 const SearchBar = ({
   input,
@@ -10,24 +11,44 @@ const SearchBar = ({
   sideButtonIsDisabled,
   addMethod,
   randomColor,
+  title,
+  searchBoxRef,
+  searchBoxIsFocused,
+  setSearchBoxIsFocused,
 }: {
   input: string;
   placeholder: string;
-  inputHandler: (input: string) => void;
+  inputHandler: any;
   clearInputHandler: () => void;
   isSideButton: boolean;
   sideButtonText?: string;
   sideButtonIsDisabled?: boolean;
-  addMethod: (input: string, e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+  addMethod?: (input: string, e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
   randomColor?: TThemeColor;
+  title?: string;
+  searchBoxRef?: React.MutableRefObject<HTMLInputElement | null>;
+  searchBoxIsFocused?: boolean;
+  setSearchBoxIsFocused?: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   return (
-    <div className="add-interests-bar">
+    <div className={styles.bar}>
       <input
+        ref={searchBoxRef}
+        title={title ? title : undefined}
         value={input}
         onChange={(e) => inputHandler(e.target.value)}
         type="text"
         placeholder={placeholder}
+        onFocus={setSearchBoxIsFocused ? () => setSearchBoxIsFocused(true) : undefined}
+        onBlur={setSearchBoxIsFocused ? () => setSearchBoxIsFocused(false) : undefined}
+        style={
+          searchBoxIsFocused
+            ? {
+                boxShadow: `0px 0px 10px 2px ${randomColor}`,
+                outline: "none",
+              }
+            : undefined
+        }
       />
       {input !== "" && (
         <i title="Clear" onClick={() => clearInputHandler()} className="fas fa-times"></i>
@@ -35,8 +56,10 @@ const SearchBar = ({
       {isSideButton && (
         <button
           onClick={(e) => {
-            addMethod(input, e);
-            clearInputHandler();
+            if (addMethod) {
+              addMethod(input, e);
+              clearInputHandler();
+            }
           }}
           disabled={sideButtonIsDisabled}
           style={{ backgroundColor: randomColor }}
