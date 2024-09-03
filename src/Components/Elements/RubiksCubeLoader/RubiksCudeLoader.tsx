ca8930ddbad97b0c485ef63dd1d4fe9ecfde560d
Ethan Groene <ethangroene@gmail.com>
2024-09-03 15:28:36 -0400

    Add,implement module stylesheet

--- src/Components/Elements/RubiksCubeLoader/RubiksCudeLoader.tsx
+++ src/Components/Elements/RubiksCubeLoader/RubiksCudeLoader.tsx
@@ -1,76 +1,240 @@
-const RubiksCubeLoader = () => {
-    return (
-<div className="my-loader">
-          <div className="rubiks-cube">
-            <div className="face front">
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-            </div>
+import styles from "./styles.module.css";
 
-            <div className="face back">
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-            </div>
-            <div className="face left">
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-            </div>
-            <div className="face right">
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-            </div>
-            <div className="face top">
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-            </div>
-            <div className="face bottom">
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-              <div style={{ background: "#ffeb3b" }} className="cube"></div>
-              <div style={{ background: "#4caf50" }} className="cube"></div>
-              <div style={{ background: "#2196f3" }} className="cube"></div>
-              <div style={{ background: "#ffffff" }} className="cube"></div>
-              <div style={{ background: "#ff3d00" }} className="cube"></div>
-            </div>
-          </div>
+const RubiksCubeLoader = () => {
+  return (
+    <div className={styles.myLoader}>
+      <div className={styles.rubiksCube}>
+        <div className={`${styles.myLoader} face front`}>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
         </div>
-    )
-}
+
+        <div className="face back">
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+        </div>
+        <div className="face left">
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+        </div>
+        <div className="face right">
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+        </div>
+        <div className="face top">
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+        </div>
+        <div className="face bottom">
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffeb3b" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#4caf50" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#2196f3" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ffffff" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+          <div
+            style={{ background: "#ff3d00" }}
+            className={`${styles.myLoader} cube`}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default RubiksCubeLoader;
