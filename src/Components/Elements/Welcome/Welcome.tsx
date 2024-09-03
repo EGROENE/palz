f9ceed6069de9dec07199b50dc5e9428cc3021e2
Ethan Groene <ethangroene@gmail.com>
2024-09-03 16:53:45 -0400

    Add,implement module stylesheet

--- src/Components/Elements/Welcome/Welcome.tsx
+++ src/Components/Elements/Welcome/Welcome.tsx
@@ -1,11 +1,12 @@
 import { useMainContext } from "../../../Hooks/useMainContext";
+import styles from "./styles.module.css";
 
 const Welcome = () => {
   const { currentUser, userCreatedAccount } = useMainContext();
 
   return (
     <div className="page-hero">
-      <div className="standalone-element welcome">
+      <div className={styles.welcomeBox}>
         <h1 className="animate__animated animate__pulse">
           {userCreatedAccount
             ? `Welcome to Palz, ${currentUser?.firstName}!`
