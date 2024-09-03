b58e6a26a96e0827d33063577a76d34fc0290865
Ethan Groene <ethangroene@gmail.com>
2024-09-03 16:43:25 -0400

    Add,implement module stylesheet

--- src/Components/Elements/UserListModal/UserListModal.tsx
+++ src/Components/Elements/UserListModal/UserListModal.tsx
@@ -1,5 +1,6 @@
 import { useMainContext } from "../../../Hooks/useMainContext";
 import { TUser, TEvent } from "../../../types";
+import styles from "./styles.module.css";
 
 /* Component contains a modal w/ background, as well as a list of users. In every user box, there is user image, name, username, & button that will eventually make it possible to message user & a button that removes user from list. To be used on event pages to show list of RSVPs & list of invitees. */
 const UserListModal = ({
@@ -31,22 +32,22 @@ const UserListModal = ({
   }
 
   return (
-    <div className="modal-background">
+    <div className={styles.modalBackground}>
       <i
         title="Close"
         onClick={() => closeModalMethod(false)}
         className="fas fa-times close-module-icon"
       ></i>
-      <div className="user-list-container">
+      <div className={styles.userListContainer}>
         <h2>{header}</h2>
         {userArray.map((user) => (
-          <div key={user.id} className="listed-user">
+          <div key={user.id} className={styles.listedUser}>
             <img
               style={{ border: `2px solid ${randomColor}` }}
               src={`${user.profileImage}`}
               alt="profile pic"
             />
-            <div className="listed-user-texts-container">
+            <div className={styles.listedUserTextsContainer}>
               <p>{`${user.firstName} ${user.lastName}`}</p>
               <p>{user.username}</p>
             </div>
