e3fcda641487f6fb13e254f60201e3eb05707460
Ethan Groene <ethangroene@gmail.com>
2024-09-03 15:44:45 -0400

    Add,implement module stylesheet

--- src/Components/Elements/Sidebar/Sidebar.tsx
+++ src/Components/Elements/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useMainContext } from "../../../Hooks/useMainContext";
 import { useUserContext } from "../../../Hooks/useUserContext";
 import { TThemeColor } from "../../../types";
+import styles from "./styles.module.css";
 
 const Sidebar = () => {
   const { currentUser, theme, toggleTheme } = useMainContext();
@@ -23,7 +24,7 @@ const Sidebar = () => {
 
   return (
     <div
-      className="sidebar"
+      className={styles.sidebar}
       style={
         theme === "dark"
           ? {
@@ -36,35 +37,38 @@ const Sidebar = () => {
       <i
         onClick={() => setShowSidebar(!showSidebar)}
         style={{ backgroundColor: randomColor }}
-        className="sidebar-icon sidebar-close fas fa-times"
+        className={`${styles.sidebarIcon} ${styles.sidebarClose} fas fa-times`}
       ></i>
       <i
         onClick={() => toggleTheme()}
         className={
           theme === "light"
-            ? "sidebar-icon sidebar-toggle-theme theme-toggle-icon fas fa-moon"
-            : "sidebar-icon sidebar-toggle-theme theme-toggle-icon fas fa-sun"
+            ? `${styles.sidebarIcon} ${styles.sidebarToggleTheme} ${styles.themeToggleIcon} fas fa-moon`
+            : `${styles.sidebarIcon} ${styles.sidebarToggleTheme} ${styles.themeToggleIcon} fas fa-sun`
         }
         style={{ backgroundColor: randomColor }}
       ></i>
-      <img className="profile-image-sidebar" src={currentUser?.profileImage} />
-      <div style={{ backgroundColor: randomColor }} className="sidebar-names-container">
+      <img className={`${styles.profileImageSidebar}`} src={currentUser?.profileImage} />
+      <div
+        style={{ backgroundColor: randomColor }}
+        className={styles.sidebarNamesContainer}
+      >
         <p>{`${currentUser?.firstName} ${currentUser?.lastName}`}</p>
         <p>{currentUser?.username}</p>
       </div>
-      <div className="sidebar-options-container">
-        <Link to={"/add-event"} className="sidebar-options">
+      <div className={styles.sidebarOptionContainer}>
+        <Link to={"/add-event"} className={styles.sidebarOption}>
           Create Event
         </Link>
-        <Link to={`/${currentUser?.username}/events`} className="sidebar-options">
+        <Link to={`/${currentUser?.username}/events`} className={styles.sidebarOption}>
           My Events
         </Link>
-        <Link className="sidebar-options" to={"/events"}>
+        <Link className={styles.sidebarOption} to={"/events"}>
           Explore Events
         </Link>
-        <p className="sidebar-options">Find Palz</p>
-        <p className="sidebar-options">My Palz</p>
-        <Link className="sidebar-options" to="/settings">
+        <p className={styles.sidebarOption}>Find Palz</p>
+        <p className={styles.sidebarOption}>My Palz</p>
+        <Link className={styles.sidebarOption} to="/settings">
           Settings
         </Link>
       </div>
