d0eb34043cdf9826fc8a60d47a00ce8d3a00527b
Ethan Groene <ethangroene@gmail.com>
2024-09-10 23:11:47 -0400

    Ensure empty friendRequests arr set in userData
    This object is passed to request to create new user in DB. friendRequests should be initialized as empty array.

--- src/Contexts/userContext.tsx
+++ src/Contexts/userContext.tsx
@@ -155,6 +155,7 @@ export const UserContextProvider = ({ children }: { children: ReactNode }) => {
     whoCanAddUserAsOrganizer: whoCanAddUserAsOrganizer,
     whoCanInviteUser: whoCanInviteUser,
     profileVisibleTo: profileVisibleTo,
+    friendRequests: [],
   };
 
   // Called when user switches b/t login & signup forms & when user logs out
