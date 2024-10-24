66e730cb9b8ddaa077010a16b72a3a3058ddc260
Ethan Groene <ethangroene@gmail.com>
2024-10-24 13:52:58 -0600

    Add method to convert img file to base64
    ...so that it can be stored as string in mongodb.

--- src/methods.tsx
+++ src/methods.tsx
@@ -142,7 +142,21 @@ const getStringArraySortedAlphabetically = (array: string[]): string[] => {
   });
 };
 
+const convertToBase64 = (file: File): Promise<unknown> => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
+
 const Methods = {
+  convertToBase64,
   arraysAreIdentical,
   sortEventsSoonestToLatest,
   isValidUrl,
