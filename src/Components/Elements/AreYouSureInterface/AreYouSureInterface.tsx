ad9955edb641e5c02e54d9d76d5bb5ce3fb4c799
Ethan Groene <ethangroene@gmail.com>
2024-11-01 22:44:12 -0600

    Add functionality to handle file upload/removal
    Also, define randomColor here now

--- src/Components/Elements/AreYouSureInterface/AreYouSureInterface.tsx
+++ src/Components/Elements/AreYouSureInterface/AreYouSureInterface.tsx
@@ -1,41 +1,90 @@
 import { TThemeColor } from "../../../types";
+import FileUploadWithButton from "../FileUploadWithButton/FileUploadWithButton";
 import styles from "./styles.module.css";
+import { useUserContext } from "../../../Hooks/useUserContext";
+import { useState, useEffect } from "react";
 
 type TAreYouSureInterfaceProps = {
-  message: string;
+  header: string;
   subheader?: string;
-  noButtonText: string;
-  yesButtonText: string;
+  buttonTwoText: string;
+  buttonOneText: string;
   setShowAreYouSureInterface: React.Dispatch<React.SetStateAction<boolean>>;
-  executionHandler: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  randomColor: TThemeColor | undefined;
+  executionHandler?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  isFileUpload?: boolean;
+  removeFile?: Function;
 };
 
 const AreYouSureInterface = ({
-  message,
+  header,
   subheader,
-  noButtonText,
-  yesButtonText,
+  buttonTwoText,
+  buttonOneText,
   setShowAreYouSureInterface,
   executionHandler,
-  randomColor,
+  isFileUpload,
+  removeFile,
 }: TAreYouSureInterfaceProps) => {
+  const { handleProfileImageUpload, profileImage } = useUserContext();
+
+  const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
+
+  useEffect(() => {
+    const themeColors: TThemeColor[] = [
+      "var(--theme-blue)",
+      "var(--theme-green)",
+      "var(--theme-pink)",
+      "var(--theme-purple)",
+      "var(--theme-orange)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+  }, []);
+
   return (
     <div className={styles.modalBackground}>
+      {isFileUpload && (
+        <i
+          title="Close"
+          onClick={() => setShowAreYouSureInterface(false)}
+          className="fas fa-times close-module-icon"
+        ></i>
+      )}
       <div className={styles.areYouSureModal}>
-        <header style={{ color: randomColor }}>{message}</header>
-        {subheader && <p>{subheader}</p>}
-        <div className={styles.buttonsContainer}>
-          <button onClick={() => setShowAreYouSureInterface(false)}>
-            {noButtonText}
-          </button>
-          <button
-            style={{ backgroundColor: randomColor }}
-            onClick={(e) => executionHandler(e)}
-          >
-            {yesButtonText}
-          </button>
-        </div>
+        {!isFileUpload ? (
+          <>
+            <header style={{ color: randomColor }}>{header}</header>
+            {subheader && <p>{subheader}</p>}
+            <div className={styles.buttonsContainer}>
+              <button onClick={() => setShowAreYouSureInterface(false)}>
+                {buttonOneText}
+              </button>
+              <button
+                style={{ backgroundColor: randomColor }}
+                onClick={executionHandler ? (e) => executionHandler(e) : undefined}
+              >
+                {buttonTwoText}
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <header style={{ color: randomColor }}>{header}</header>
+            {subheader && <p>{subheader}</p>}
+            <div className={styles.buttonsContainer}>
+              {profileImage !== "" && (
+                <button onClick={removeFile ? (e) => removeFile(e) : undefined}>
+                  {buttonTwoText}
+                </button>
+              )}
+              <FileUploadWithButton
+                addFile={handleProfileImageUpload}
+                addFileButtonText="Upload New"
+                randomColor={randomColor}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
