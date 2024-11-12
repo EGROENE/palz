8ad873dd2c5ece5f8e90e99e1b532fc5b76cca34
Ethan Groene <ethangroene@gmail.com>
2024-11-12 15:01:20 -0600

    Add FriendRequests page, styling

---
+++ src/Components/Pages/FriendRequests/FriendRequests.tsx
@@ -0,0 +1,165 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useMainContext } from "../../../Hooks/useMainContext";
+import { useUserContext } from "../../../Hooks/useUserContext";
+import { TUser, TThemeColor } from "../../../types";
+import ListedUser from "../../Elements/ListedUser/ListedUser";
+import styles from "./styles.module.css";
+
+const FriendRequests = () => {
+  const { allUsers } = useMainContext();
+  const {
+    showSidebar,
+    setShowSidebar,
+    handleAcceptFriendRequest,
+    handleRejectFriendRequest,
+  } = useUserContext();
+  const { username } = useParams();
+
+  const [requestsVisible, setRequestsVisible] = useState<"sent" | "received" | null>(
+    null
+  );
+
+  const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
+
+  const currentUser: TUser = allUsers.filter((user) => user.username === username)[0];
+
+  const usersWhoSentCurrentUserARequest: TUser[] = allUsers.filter(
+    (user) => user._id && currentUser.friendRequestsReceived.includes(user._id)
+  );
+
+  const usersToWhomCurrentUserSentRequest: TUser[] = allUsers.filter(
+    (user) => user._id && currentUser.friendRequestsSent.includes(user._id)
+  );
+
+  const userHasPendingRequests: boolean =
+    currentUser.friendRequestsReceived.length > 0 ||
+    currentUser.friendRequestsSent.length > 0;
+
+  useEffect(() => {
+    if (showSidebar) {
+      setShowSidebar(false);
+    }
+
+    const themeColors: TThemeColor[] = [
+      "var(--theme-blue)",
+      "var(--theme-green)",
+      "var(--theme-pink)",
+      "var(--theme-purple)",
+      "var(--theme-orange)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+
+    if (
+      currentUser.friendRequestsReceived.length > 0 &&
+      currentUser.friendRequestsSent.length > 0
+    ) {
+      setRequestsVisible("sent");
+    } else {
+      if (currentUser.friendRequestsReceived.length > 0) {
+        setRequestsVisible("received");
+      }
+      if (currentUser.friendRequestsSent.length > 0) {
+        setRequestsVisible("sent");
+      }
+    }
+  }, []);
+
+  return (
+    <div className="page-hero" onClick={() => showSidebar && setShowSidebar(false)}>
+      <h1>Friend Requests</h1>
+      {userHasPendingRequests === null ? (
+        <div className="page-hero standalone-element">
+          <h1>No pending friend requests</h1>
+        </div>
+      ) : (
+        <>
+          <div className={styles.friendRequestFilterHeaders}>
+            {usersToWhomCurrentUserSentRequest.length > 0 && (
+              <div>
+                <header
+                  style={
+                    requestsVisible === "sent" &&
+                    usersWhoSentCurrentUserARequest.length > 0
+                      ? { color: randomColor }
+                      : { color: "var(--text-color)" }
+                  }
+                  onClick={
+                    usersWhoSentCurrentUserARequest.length > 0
+                      ? () => setRequestsVisible("sent")
+                      : undefined
+                  }
+                >
+                  Sent
+                </header>
+                {requestsVisible === "sent" &&
+                  usersWhoSentCurrentUserARequest.length > 0 && (
+                    <div
+                      className={`${styles.requestTypeUnderline} animate__animated animate__slideInRight`}
+                      style={{ backgroundColor: randomColor }}
+                    ></div>
+                  )}
+              </div>
+            )}
+            {usersWhoSentCurrentUserARequest.length > 0 && (
+              <div>
+                <header
+                  style={
+                    requestsVisible === "received" &&
+                    usersToWhomCurrentUserSentRequest.length > 0
+                      ? { color: randomColor }
+                      : { color: "var(--text-color)" }
+                  }
+                  onClick={
+                    usersToWhomCurrentUserSentRequest.length > 0
+                      ? () => setRequestsVisible("received")
+                      : undefined
+                  }
+                >
+                  Received
+                </header>
+                {requestsVisible === "received" &&
+                  usersToWhomCurrentUserSentRequest.length > 0 && (
+                    <div
+                      className={`${styles.requestTypeUnderline} animate__animated animate__slideInLeft`}
+                      style={{ backgroundColor: randomColor }}
+                    ></div>
+                  )}
+              </div>
+            )}
+          </div>
+          <div className={styles.friendRequestUsersContainer}>
+            {requestsVisible === "sent"
+              ? usersToWhomCurrentUserSentRequest.map((user) => (
+                  <ListedUser
+                    key={user._id}
+                    user={user}
+                    randomColor={randomColor}
+                    //buttonOneHandler={}
+                    //buttonTwoHandler={}
+                    buttonOneText="See Profile"
+                    buttonTwoText="Retract"
+                  />
+                ))
+              : usersWhoSentCurrentUserARequest.length > 0 &&
+                usersWhoSentCurrentUserARequest.map((user) => (
+                  <ListedUser
+                    key={user._id}
+                    user={user}
+                    randomColor={randomColor}
+                    buttonOneText="Accept"
+                    buttonTwoText="Reject"
+                    buttonOneHandler={handleAcceptFriendRequest}
+                    buttonOneHandlerParams={[user, currentUser]}
+                    buttonTwoHandler={handleRejectFriendRequest}
+                    buttonTwoHandlerParams={[user, currentUser]}
+                  />
+                ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+export default FriendRequests;
