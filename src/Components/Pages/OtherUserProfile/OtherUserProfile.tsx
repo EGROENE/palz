4244f1e100094b38de2e3cc3e906701a43615b99
Ethan Groene <ethangroene@gmail.com>
2024-11-19 00:37:58 -0600

    Get currentOtherUser based on param
    ...not on universal state value currentOtherUser, since that would cause info that doesn't match user in url to appear on OtherUserProfile if OtherUserProfile is rendered for different users.

--- src/Components/Pages/OtherUserProfile/OtherUserProfile.tsx
+++ src/Components/Pages/OtherUserProfile/OtherUserProfile.tsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
 import { useUserContext } from "../../../Hooks/useUserContext";
 
 const OtherUserProfile = () => {
-  const { currentOtherUser } = useUserContext();
+  const { allUsers } = useUserContext();
+  const { username } = useParams();
+  const currentOtherUser = allUsers.filter((user) => user.username === username)[0];
 
   // if currentUser is undefined on initial render, redirect to login page
 
