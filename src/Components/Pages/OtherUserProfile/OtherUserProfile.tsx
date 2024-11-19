ee4ff3f9d2efc31ddbbc164f9d4299956119a993
Ethan Groene <ethangroene@gmail.com>
2024-11-19 00:43:38 -0600

    Redirect to login pg if nobody signed in

--- src/Components/Pages/OtherUserProfile/OtherUserProfile.tsx
+++ src/Components/Pages/OtherUserProfile/OtherUserProfile.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useUserContext } from "../../../Hooks/useUserContext";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const OtherUserProfile = () => {
-  const { allUsers } = useUserContext();
+  const navigation = useNavigate();
+  const { allUsers, currentUser, userCreatedAccount } = useUserContext();
   const { username } = useParams();
   const currentOtherUser = allUsers.filter((user) => user.username === username)[0];
 
   // if currentUser is undefined on initial render, redirect to login page
+  useEffect(() => {
+    if (!currentUser && userCreatedAccount === null) {
+      toast.error("Please login before accessing this page");
+      navigation("/");
+    }
+  }, [currentUser, navigation, userCreatedAccount]);
 
   return <h1>{currentOtherUser?.firstName}</h1>;
 };
