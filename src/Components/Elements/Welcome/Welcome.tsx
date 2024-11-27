40b0b4b2b98c86c97b2092452931235838eb72cd
Ethan Groene <ethangroene@gmail.com>
2024-11-27 00:51:53 -0600

    ensure most-current first,last name used
    currentUser should set in time as someone creates account, but this covers in case state is slow in setting.

--- src/Components/Elements/Welcome/Welcome.tsx
+++ src/Components/Elements/Welcome/Welcome.tsx
@@ -2,15 +2,15 @@ import { useUserContext } from "../../../Hooks/useUserContext";
 import styles from "./styles.module.css";
 
 const Welcome = () => {
-  const { currentUser, userCreatedAccount } = useUserContext();
+  const { userCreatedAccount, firstName, lastName } = useUserContext();
 
   return (
     <div className="page-hero">
       <div className={styles.welcomeBox}>
         <header className="animate__animated animate__pulse">
           {userCreatedAccount
-            ? `Welcome to Palz, ${currentUser?.firstName}!`
-            : `Welcome back, ${currentUser?.firstName}!`}
+            ? `Welcome to Palz, ${firstName}!`
+            : `Welcome back, ${lastName}!`}
         </header>
       </div>
     </div>
