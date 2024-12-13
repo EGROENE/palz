948daee3c65210fec9278c7898cffe0b156a4c73
Ethan Groene <ethangroene@gmail.com>
2024-12-13 16:59:05 -0500

    Limit,auto increment displayed items
    User may now pass in how many they want to display originally & an increment by which they want the display count to increase. Once user scrolls to bottom of list, display count is incremented.

--- src/Components/Elements/DropdownChecklist/DropdownChecklist.tsx
+++ src/Components/Elements/DropdownChecklist/DropdownChecklist.tsx
@@ -8,20 +8,54 @@ const DropdownChecklist = ({
   storageArray,
   setStorageArray,
   displayCount,
+  setDisplayCount,
+  displayCountInterval,
   event,
 }: {
   displayedItemsArray: TUser[]; // type can be changed later if used for non-user lists
   storageArray: string[];
   setStorageArray: React.Dispatch<React.SetStateAction<string[]>>;
   displayCount?: number;
+  setDisplayCount?: React.Dispatch<React.SetStateAction<number>>;
+  displayCountInterval?: number;
   event?: TEvent;
 }) => {
   const { isLoading } = useMainContext();
+
   const { handleAddRemoveUserAsOrganizer } = useUserContext();
 
+  let displayedItemsArrayFiltered: TUser[] = [];
+  if (displayCount) {
+    for (let i = 0; i < displayCount; i++) {
+      displayedItemsArrayFiltered.push(displayedItemsArray[i]);
+    }
+  } else {
+    displayedItemsArrayFiltered = displayedItemsArray;
+  }
+
+  const handleScroll = (e: React.UIEvent<HTMLUListElement, UIEvent>): void => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLElement;
+    const bottomReached = scrollTop + clientHeight === scrollHeight;
+    if (displayCount && displayCountInterval && setDisplayCount) {
+      if (bottomReached) {
+        if (
+          displayedItemsArray.length - displayedItemsArrayFiltered.length >=
+          displayCountInterval
+        ) {
+          setDisplayCount(displayCount + displayCountInterval);
+        } else {
+          setDisplayCount(
+            displayCount +
+              (displayedItemsArray.length - displayedItemsArrayFiltered.length)
+          );
+        }
+      }
+    }
+  };
+
   return (
-    <ul className="dropdown-list">
-      {displayedItemsArray.map((user) => (
+    <ul onScroll={(e) => handleScroll(e)} className="dropdown-list">
+      {displayedItemsArrayFiltered.map((user) => (
         <div
           key={user._id}
           onClick={(e) =>
