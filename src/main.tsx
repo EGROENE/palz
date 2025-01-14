9c2096e5c855fa22a5b8301b1eb68342f8178d9d
Ethan Groene <ethangroene@gmail.com>
2025-01-14 14:39:48 -0600

    Wrap entire app inside QueryClientProvider
    Also, insert ReactQueryDevTools component

--- src/main.tsx
+++ src/main.tsx
@@ -9,26 +9,31 @@ import { MainContextProvider } from "./Contexts/mainContext.tsx";
 import { UserContextProvider } from "./Contexts/userContext.tsx";
 import { EventContextProvider } from "./Contexts/eventContext.tsx";
 import ErrorBoundary from "./Components/Pages/ErrorBoundary/ErrorBoundary.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MainContextProvider>
-        <Toaster
-          toastOptions={{
-            style: {
-              fontFamily: "var(--text-font)",
-            },
-          }}
-        />
-        <UserContextProvider>
-          <EventContextProvider>
-            <ErrorBoundary>
-              <App />
-            </ErrorBoundary>
-          </EventContextProvider>
-        </UserContextProvider>
-      </MainContextProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={new QueryClient()}>
+      <BrowserRouter>
+        <MainContextProvider>
+          <Toaster
+            toastOptions={{
+              style: {
+                fontFamily: "var(--text-font)",
+              },
+            }}
+          />
+          <UserContextProvider>
+            <EventContextProvider>
+              <ErrorBoundary>
+                <App />
+                <ReactQueryDevtools />
+              </ErrorBoundary>
+            </EventContextProvider>
+          </UserContextProvider>
+        </MainContextProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
