7fe347efe3eac4e10452c31e6bd53435e116c7f0
Ethan Groene <ethangroene@gmail.com>
2025-02-18 11:57:01 -0600

    Add patch route for updateMessage

--- backend/routes/messages.cjs
+++ backend/routes/messages.cjs
@@ -4,6 +4,7 @@ const {
   getCurrentUserMessages,
   createMessage,
   deleteMessage,
+  updateMessage,
 } = require("../controllers/messageControllers");
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/:currentUserID", getCurrentUserMessages);
 router.post("/", createMessage);
 router.delete("/:messageID", deleteMessage);
+router.patch("/:messageID", updateMessage);
 
 module.exports = router;
