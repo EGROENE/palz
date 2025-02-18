2fd5734f8dd37f6e7582ab906ca826d9bb8eb26c
Ethan Groene <ethangroene@gmail.com>
2025-02-18 11:09:22 -0600

    Add delete route for deleteMessage controller

--- backend/routes/messages.cjs
+++ backend/routes/messages.cjs
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   getCurrentUserMessages,
   createMessage,
+  deleteMessage,
 } = require("../controllers/messageControllers");
 
 const router = express.Router();
 
 router.get("/:currentUserID", getCurrentUserMessages);
 router.post("/", createMessage);
+router.delete("/:messageID", deleteMessage);
 
 module.exports = router;
