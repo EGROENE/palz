3912972ca194d9981f798ac6feef12d03ad79aca
Ethan Groene <ethangroene@gmail.com>
2025-02-19 21:21:31 -0600

    Del _id prop from chatSchema
    Had mistakenly been added here instead of to messagemodel (which is not needed), since a unique ObjectId will have to be generated for new messages

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -30,10 +30,6 @@ const Schema = mongoose.Schema;
 }); */
 
 const chatSchema = new Schema({
-  _id: {
-    type: ObjectId,
-    required: true,
-  },
   members: {
     type: [String],
     required: true,
