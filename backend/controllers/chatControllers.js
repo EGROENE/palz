1331022af24efeeed5875d1eb4d05bf1e1c0f298
Ethan Groene <ethangroene@gmail.com>
2025-02-19 20:16:04 -0600

    Del unneeded import

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-const Message = require("../models/messageModel");
-
 const Chat = require("../models/chatModel");
 
 // get all chats in which user is member:
