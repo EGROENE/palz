8a2194d0533b1fd145754d261d8f7c5e72d9ed0d
Ethan Groene <ethangroene@gmail.com>
2025-02-25 11:55:17 -0600

    Show when most-recent msg was sent

--- src/Components/Elements/ChatPreview/ChatPreview.tsx
+++ src/Components/Elements/ChatPreview/ChatPreview.tsx
@@ -49,6 +49,55 @@ const ChatPreview = ({ chat }: { chat: TChat }) => {
     return unreadMessages.length >= 10 ? "9+" : unreadMessages.length;
   };
 
+  const getTimeOfLastMessageInChat = (chat: TChat): string => {
+    const now = Date.now();
+    const timeOfMostRecentMessage = chat.messages[chat.messages.length - 1].timeSent;
+    const timeElapsed = now - timeOfMostRecentMessage;
+    const dateOfLastMessage = new Date(timeOfMostRecentMessage);
+
+    const oneDay = 1000 * 60 * 60 * 24;
+    const oneWeek = oneDay * 7;
+    const oneYear = oneDay * 365;
+
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    if (timeElapsed < oneDay) {
+      return dateOfLastMessage.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    }
+
+    if (timeElapsed > oneDay && timeElapsed < oneWeek) {
+      const dayOfWeek = dateOfLastMessage.getDay();
+      return days[dayOfWeek];
+    }
+    if (timeElapsed > oneWeek && timeElapsed < oneYear) {
+      return months[dateOfLastMessage.getMonth()] + " " + dateOfLastMessage.getDate();
+    }
+    return (
+      months[dateOfLastMessage.getMonth()] +
+      " " +
+      dateOfLastMessage.getDate() +
+      " " +
+      dateOfLastMessage.getFullYear()
+    );
+  };
+
   return (
     <div
       key={chat._id}
@@ -97,7 +146,10 @@ const ChatPreview = ({ chat }: { chat: TChat }) => {
               : ""
           }`}
         </header>
-        <p>{getPreviewOfLastMessage(chat)}</p>
+        <div className="last-message-preview-and-date">
+          <span>{getPreviewOfLastMessage(chat)}</span>
+          <span>{getTimeOfLastMessageInChat(chat)}</span>
+        </div>
       </div>
     </div>
   );
