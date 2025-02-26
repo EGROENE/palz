2940077486b3d0fa6cf6022fad2a67a2c5f7256f
Ethan Groene <ethangroene@gmail.com>
2025-02-26 11:32:22 -0600

    Give messages-container a randomColor border

--- src/Components/Elements/ChatModal/ChatModal.tsx
+++ src/Components/Elements/ChatModal/ChatModal.tsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
 import { useChatContext } from "../../../Hooks/useChatContext";
 import Message from "../Message/Message";
+import { TThemeColor } from "../../../types";
 
 const ChatModal = () => {
   const { setShowChatModal, setCurrentChat, currentChat } = useChatContext();
+
+  const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
+
+  useEffect(() => {
+    // Set color of event card's border randomly:
+    const themeColors: TThemeColor[] = [
+      "var(--primary-color)",
+      "var(--secondary-color)",
+      "var(--tertiary-color)",
+      "var(--fourth-color)",
+      "var(--fifth-color)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+  }, []);
 
   return (
     <div className="modal-background">
@@ -14,7 +31,7 @@ const ChatModal = () => {
         }}
         className="fas fa-times close-module-icon"
       ></i>
-      <div className="messages-container">
+      <div style={{ border: `3px solid ${randomColor}` }} className="messages-container">
         {currentChat &&
           currentChat.messages.map((message) => (
             <Message key={message._id} message={message} />
