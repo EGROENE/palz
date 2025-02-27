d3eb11ec9afe12c2c6ba914532141df8023599e7
Ethan Groene <ethangroene@gmail.com>
2025-02-27 14:34:13 -0600

    Def randomColor,add 'new chat' btn

--- src/Components/Pages/ChatsPage/ChatsPage.tsx
+++ src/Components/Pages/ChatsPage/ChatsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMainContext } from "../../../Hooks/useMainContext";
 import { useUserContext } from "../../../Hooks/useUserContext";
 import toast from "react-hot-toast";
@@ -7,7 +7,7 @@ import { useEventContext } from "../../../Hooks/useEventContext";
 import QueryLoadingOrError from "../../Elements/QueryLoadingOrError/QueryLoadingOrError";
 import ChatPreview from "../../Elements/ChatPreview/ChatPreview";
 import Methods from "../../../methods";
-import { TChat } from "../../../types";
+import { TChat, TThemeColor } from "../../../types";
 import { useChatContext } from "../../../Hooks/useChatContext";
 import ChatModal from "../../Elements/ChatModal/ChatModal";
 
@@ -16,6 +16,21 @@ const ChatsPage = () => {
   const { currentUser, userCreatedAccount, fetchAllUsersQuery } = useUserContext();
   const { fetchAllEventsQuery } = useEventContext();
   const { fetchChatsQuery, userChats, showChatModal } = useChatContext();
+
+  const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
+
+  useEffect(() => {
+    // Set color of event card's border randomly:
+    const themeColors: TThemeColor[] = [
+      "var(--primary-color)",
+      "var(--secondary-color)",
+      "var(--tertiary-color)",
+      "var(--fourth-color)",
+      "var(--fifth-color)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+  }, []);
 
   const navigation = useNavigate();
 
@@ -67,11 +82,23 @@ const ChatsPage = () => {
         errorMessage="Error fetching your chats"
       />
       {isNoFetchError && !fetchIsLoading && userChats && (
-        <div className="chats-container">
-          {userChatsSortedMostRecent.map((chat) => (
-            <ChatPreview key={chat._id} chat={chat} />
-          ))}
-        </div>
+        <>
+          <div className="chats-container">
+            {userChatsSortedMostRecent.map((chat) => (
+              <ChatPreview key={chat._id} chat={chat} />
+            ))}
+          </div>
+          <i
+            style={
+              randomColor === "var(--primary-color)"
+                ? { backgroundColor: `${randomColor}`, color: "black" }
+                : { backgroundColor: `${randomColor}`, color: "white" }
+            }
+            id="new-chat-btn"
+            className="fas fa-feather"
+            title="Start new chat"
+          ></i>
+        </>
       )}
     </div>
   );
