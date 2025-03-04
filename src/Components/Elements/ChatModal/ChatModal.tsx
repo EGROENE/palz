3ece0338a99a5bc9db311988ff624c0bdc1abbf8
Ethan Groene <ethangroene@gmail.com>
2025-03-03 23:36:14 -0600

    Add infrastructure to add CMs
    UI, except for cancel/add btns there, including proper rendering of sections of ChatModal & resetting of vars related to chat members, is done. Requests made to DB still need to be added to handlers.

--- src/Components/Elements/ChatModal/ChatModal.tsx
+++ src/Components/Elements/ChatModal/ChatModal.tsx
@@ -3,14 +3,38 @@ import { useChatContext } from "../../../Hooks/useChatContext";
 import Message from "../Message/Message";
 import { TThemeColor } from "../../../types";
 import Tab from "../Tab/Tab";
+import SearchAndDropdownList from "../SearchAndDropdownList/SearchAndDropdownList";
+import { useUserContext } from "../../../Hooks/useUserContext";
+import DropdownChecklist from "../DropdownChecklist/DropdownChecklist";
 
 const ChatModal = () => {
-  const { setShowChatModal, setCurrentChat, currentChat, getChatMembers } =
-    useChatContext();
+  const { allOtherUsers, currentUser } = useUserContext();
+  const {
+    setShowChatModal,
+    setCurrentChat,
+    currentChat,
+    getChatMembers,
+    chatMembersSearchQuery,
+    setChatMembersSearchQuery,
+    showPotentialChatMembers,
+    setShowPotentialChatMembers,
+    getCurrentOtherUserFriends,
+    setPotentialChatMembers,
+    usersToAddToChat,
+    handleRemoveUserFromChat,
+    handleAddRemoveUserFromChat,
+    potentialChatMembers,
+    setUsersToAddToChat,
+    numberOfPotentialChatMembersDisplayed,
+    setNumberOfPotentialChatMembersDisplayed,
+    handleSearchChatMembersInput,
+  } = useChatContext();
 
   const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
 
   const [showMembers, setShowMembers] = useState<boolean>(false);
+
+  const [showAddMemberModal, setShowAddMemberModal] = useState<boolean>(false);
 
   const [inputMessage, setInputMessage] = useState<string>("");
 
@@ -27,51 +51,176 @@ const ChatModal = () => {
     setRandomColor(themeColors[randomNumber]);
   }, []);
 
+  const initiatePotentialChatMembers = (): void => {
+    setPotentialChatMembers(
+      allOtherUsers.filter((otherUser) => {
+        const userIsNotAlreadyInCurrentChat: boolean =
+          otherUser._id && currentChat && currentChat.members.includes(otherUser._id)
+            ? false
+            : true;
+
+        const currentUserIsBlocked: boolean =
+          currentUser && currentUser._id
+            ? otherUser.blockedUsers.includes(currentUser._id)
+            : false;
+
+        const currentUserIsFriendOfFriend: boolean =
+          currentUser && currentUser._id
+            ? getCurrentOtherUserFriends(otherUser).some(
+                (otherUserFriend) =>
+                  currentUser._id && otherUserFriend.friends.includes(currentUser._id)
+              )
+            : false;
+
+        const currentUserIsFriend: boolean =
+          currentUser && currentUser._id
+            ? otherUser.friends.includes(currentUser._id)
+            : false;
+
+        if (
+          userIsNotAlreadyInCurrentChat &&
+          !currentUserIsBlocked &&
+          (otherUser.whoCanMessage === "anyone" ||
+            (otherUser.whoCanMessage === "friends" && currentUserIsFriend) ||
+            (otherUser.whoCanMessage === "friends of friends" &&
+              currentUserIsFriendOfFriend))
+        ) {
+          return otherUser;
+        }
+      })
+    );
+  };
+
+  useEffect(() => {
+    initiatePotentialChatMembers();
+  }, [usersToAddToChat]);
+
+  const handleCancelAddingChatMembers = (
+    e:
+      | React.MouseEvent<HTMLButtonElement, MouseEvent>
+      | React.MouseEvent<HTMLElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    setUsersToAddToChat([]);
+    setChatMembersSearchQuery("");
+    initiatePotentialChatMembers();
+    setShowAddMemberModal(false);
+    setShowPotentialChatMembers(false);
+  };
+
   return (
     <div className="modal-background">
       <i
         title="Close"
-        onClick={() => {
+        onClick={(e) => {
           setShowChatModal(false);
           setCurrentChat(null);
+          if (showAddMemberModal) {
+            handleCancelAddingChatMembers(e);
+          }
         }}
         className="fas fa-times close-module-icon"
       ></i>
       <div style={{ border: `3px solid ${randomColor}` }} className="chat-container">
-        <div
-          style={
-            !showMembers
-              ? { borderBottom: `2px solid ${randomColor}`, height: "unset" }
-              : {
-                  borderBottom: `2px solid ${randomColor}`,
-                  height: "17%",
-                  paddingBottom: "1rem",
-                }
-          }
-          className="members-panel"
-        >
-          {currentChat && currentChat.chatName && currentChat.chatName !== "" && (
-            <p>{currentChat.chatName}</p>
-          )}
-          {!showMembers && <p onClick={() => setShowMembers(true)}>Show members</p>}
-          {currentChat && showMembers && (
-            <>
-              <div className="members-container">
-                {getChatMembers(currentChat.members).map((member) => (
+        {showAddMemberModal && (
+          <div className="add-members-modal">
+            <i
+              title="Close"
+              onClick={(e) => {
+                handleCancelAddingChatMembers(e);
+              }}
+              className="fas fa-times close-module-icon"
+            ></i>
+            <header>Add member to chat:</header>
+            {usersToAddToChat.length > 0 && (
+              <div className="added-user-tab-container">
+                {usersToAddToChat.map((user) => (
                   <Tab
-                    userMayNotDelete={true}
-                    key={member._id}
+                    key={user._id}
+                    info={user}
+                    removeHandler={() => handleRemoveUserFromChat(user)}
                     randomColor={randomColor}
-                    info={member}
+                    userMayNotDelete={false}
                   />
                 ))}
-                <i className="fas fa-plus"></i>
               </div>
-              <p onClick={() => setShowMembers(false)}>Hide members</p>
-            </>
-          )}
-        </div>
-        {currentChat && (
+            )}
+            <SearchAndDropdownList
+              name="add-member-from-chat"
+              id="add-member-from-chat"
+              placeholder="Search users by username, first/last names"
+              query={chatMembersSearchQuery}
+              clearQueryOnClick={() => {
+                setChatMembersSearchQuery("");
+                initiatePotentialChatMembers();
+              }}
+              randomColor={randomColor}
+              showList={showPotentialChatMembers}
+              setShowList={setShowPotentialChatMembers}
+              inputOnChange={(e) =>
+                handleSearchChatMembersInput(
+                  e,
+                  showPotentialChatMembers,
+                  setShowPotentialChatMembers,
+                  allOtherUsers,
+                  initiatePotentialChatMembers
+                )
+              }
+              dropdownChecklist={
+                <DropdownChecklist
+                  usedFor="potential-additional-chat-members"
+                  action={handleAddRemoveUserFromChat}
+                  actionEventParamNeeded={false}
+                  displayedItemsArray={potentialChatMembers}
+                  storageArray={usersToAddToChat.map((user) => user._id)}
+                  setStorageArray={setUsersToAddToChat}
+                  displayedItemsCount={numberOfPotentialChatMembersDisplayed}
+                  setDisplayedItemsCount={setNumberOfPotentialChatMembersDisplayed}
+                  displayedItemsCountInterval={10}
+                />
+              }
+            />
+          </div>
+        )}
+        {!showAddMemberModal && (
+          <div
+            style={
+              !showMembers
+                ? { borderBottom: `2px solid ${randomColor}`, height: "unset" }
+                : {
+                    borderBottom: `2px solid ${randomColor}`,
+                    height: "17%",
+                    paddingBottom: "1rem",
+                  }
+            }
+            className="members-panel"
+          >
+            {currentChat && currentChat.chatName && currentChat.chatName !== "" && (
+              <p>{currentChat.chatName}</p>
+            )}
+            {!showMembers && <p onClick={() => setShowMembers(true)}>Show members</p>}
+            {currentChat && showMembers && (
+              <>
+                <div className="members-container">
+                  {getChatMembers(currentChat.members).map((member) => (
+                    <Tab
+                      userMayNotDelete={true}
+                      key={member._id}
+                      randomColor={randomColor}
+                      info={member}
+                    />
+                  ))}
+                  <i
+                    onClick={() => setShowAddMemberModal(true)}
+                    className="fas fa-plus"
+                  ></i>
+                </div>
+                <p onClick={() => setShowMembers(false)}>Hide members</p>
+              </>
+            )}
+          </div>
+        )}
+        {currentChat && !showAddMemberModal && (
           <div className="messages-container">
             {currentChat.messages.map((message) => (
               <Message
@@ -82,22 +231,24 @@ const ChatModal = () => {
             ))}
           </div>
         )}
-        <div className="message-input-container">
-          <textarea
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.target.value)}
-            placeholder="Type message"
-            maxLength={10000}
-          ></textarea>
-          <i
-            style={
-              randomColor === "var(--primary-color)"
-                ? { backgroundColor: `${randomColor}`, color: "black" }
-                : { backgroundColor: `${randomColor}`, color: "white" }
-            }
-            className="fas fa-paper-plane"
-          ></i>
-        </div>
+        {!showAddMemberModal && (
+          <div className="message-input-container">
+            <textarea
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
+              placeholder="Type message"
+              maxLength={10000}
+            ></textarea>
+            <i
+              style={
+                randomColor === "var(--primary-color)"
+                  ? { backgroundColor: `${randomColor}`, color: "black" }
+                  : { backgroundColor: `${randomColor}`, color: "white" }
+              }
+              className="fas fa-paper-plane"
+            ></i>
+          </div>
+        )}
       </div>
     </div>
   );
