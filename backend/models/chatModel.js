6c6d36c5c6be74cf46ef8dc7340a6bb7e7bc85a3
Ethan Groene <ethangroene@gmail.com>
2025-03-08 13:08:12 -0600

    Add _id prop to chatSchema

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -30,6 +30,10 @@ const Schema = mongoose.Schema;
 }); */
 
 const chatSchema = new Schema({
+  _id: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  },
   members: {
     type: [String],
     required: true,
