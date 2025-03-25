68ed301ea6880a13b799c1ae2a88ac836e5f436a
Ethan Groene <ethangroene@gmail.com>
2025-03-25 16:31:30 -0600

    If randomColor,add line above footer
    Also, style this line

--- src/Components/Elements/Footer/Footer.tsx
+++ src/Components/Elements/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.css";
 import SiteLinks from "../SiteLinks/SiteLinks";
+import { TThemeColor } from "../../../types";
 
-const Footer = () => {
+const Footer = ({ randomColor }: { randomColor?: TThemeColor }) => {
   const now: Date = new Date();
   return (
     <div className={styles.footer}>
+      {randomColor && <hr style={{ border: `1px solid ${randomColor}`, width: "75%" }} />}
       <SiteLinks />
       <footer>&copy; {`Palz ${now.getFullYear()}`}</footer>
     </div>
