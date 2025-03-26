49764b27ee163f4b9730a68830384b6ffe66af4f
Ethan Groene <ethangroene@gmail.com>
2025-03-26 13:46:19 -0600

    Def borderColor here
    Makes randomColor parameter unnecessary.

--- src/Components/Elements/Footer/Footer.tsx
+++ src/Components/Elements/Footer/Footer.tsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import SiteLinks from "../SiteLinks/SiteLinks";
 import { TThemeColor } from "../../../types";
 
-const Footer = ({ randomColor }: { randomColor?: TThemeColor }) => {
+const Footer = () => {
+  const [footerBorderColor, setFooterBorderColor] = useState<TThemeColor | undefined>();
+
+  useEffect(() => {
+    // Set color of event card's border randomly:
+    const themeColors: TThemeColor[] = [
+      "var(--primary-color)",
+      "var(--secondary-color)",
+      "var(--tertiary-color)",
+      "var(--fourth-color)",
+      "var(--fifth-color)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setFooterBorderColor(themeColors[randomNumber]);
+  }, []);
+
   const now: Date = new Date();
   return (
     <div className={styles.footer}>
-      {randomColor && <hr style={{ border: `1px solid ${randomColor}`, width: "75%" }} />}
+      {<hr style={{ border: `1px solid ${footerBorderColor}`, width: "75%" }} />}
       <SiteLinks />
       <footer>&copy; {`Palz ${now.getFullYear()}`}</footer>
     </div>
