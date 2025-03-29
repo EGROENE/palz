e4bfc98f605a9659cd458beb54a020628b23b2f1
Ethan Groene <ethangroene@gmail.com>
2025-03-29 12:46:47 -0600

    Add EditChatNameModal

---
+++ src/Components/EditChatNameModal/EditChatNameModal.tsx
@@ -0,0 +1,83 @@
+import { useChatContext } from "../../Hooks/useChatContext";
+import { useState, useEffect } from "react";
+import { TThemeColor } from "../../types";
+
+const EditChatNameModal = () => {
+  const {
+    currentChat,
+    chatName,
+    setChatName,
+    handleChatNameInput,
+    chatNameError,
+    setShowEditChatNameModal,
+    handleUpdateChatName,
+  } = useChatContext();
+
+  const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
+
+  useEffect(() => {
+    // Set color of event card's border randomly:
+    const themeColors: TThemeColor[] = [
+      "var(--primary-color)",
+      "var(--secondary-color)",
+      "var(--tertiary-color)",
+      "var(--fourth-color)",
+      "var(--fifth-color)",
+    ];
+    const randomNumber = Math.floor(Math.random() * themeColors.length);
+    setRandomColor(themeColors[randomNumber]);
+  }, []);
+
+  const handleCancelEditChatName = (
+    e:
+      | React.MouseEvent<HTMLButtonElement, MouseEvent>
+      | React.MouseEvent<HTMLElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    setChatName(undefined);
+    setShowEditChatNameModal(false);
+  };
+
+  return (
+    <div className="modal-background">
+      <i
+        title="Close"
+        onClick={(e) => {
+          handleCancelEditChatName(e);
+        }}
+        className="fas fa-times close-module-icon"
+      ></i>
+
+      <header>
+        {currentChat && currentChat.chatName
+          ? "Change name of group chat"
+          : "Add name to group chat"}
+      </header>
+      <input
+        value={chatName ? chatName : ""}
+        onChange={(e) => handleChatNameInput(e)}
+        type="text"
+        placeholder="Choose a name for the group chat"
+      ></input>
+      {chatNameError !== "" && <p>{chatNameError}</p>}
+      <div className="create-new-chat-modal-buttons">
+        <button onClick={(e) => handleCancelEditChatName(e)} id="cancel">
+          Cancel
+        </button>
+        <button
+          onClick={currentChat ? () => handleUpdateChatName(currentChat) : undefined}
+          disabled={chatName === undefined || chatName.replace(/\s/g, "") === ""}
+          style={
+            randomColor === "var(--primary-color)"
+              ? { backgroundColor: `${randomColor}`, color: "black" }
+              : { backgroundColor: `${randomColor}`, color: "white" }
+          }
+        >
+          {currentChat && currentChat.chatName ? "Update Chat Name" : "Add Chat Name"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EditChatNameModal;
