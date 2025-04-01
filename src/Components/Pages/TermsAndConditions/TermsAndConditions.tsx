449707c95a83a10ea242031f2bc477ed6e927081
Ethan Groene <ethangroene@gmail.com>
2025-03-31 22:24:09 -0600

    Add <TermsAndConditions />

---
+++ src/Components/Pages/TermsAndConditions/TermsAndConditions.tsx
@@ -0,0 +1,51 @@
+const TermsAndConditions = () => {
+  return (
+    <div className="page-hero">
+      <h1>Terms & Conditions</h1>
+      <div className="terms-and-conditions-paragraphs-container">
+        <p>
+          So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der
+          Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur
+          Hölle!
+        </p>
+        <p>
+          Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was
+          denken lassen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu
+          blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu
+          sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu sprechen
+          Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin.
+        </p>
+        <p>
+          Ich bin von je der Ordnung Freund gewesen. Ich höre schon des Dorfs Getümmel,
+          Hier ist des Volkes wahrer Himmel, Zufrieden jauchzet groß und klein, Hier bin
+          ich nicht; doch viel ist mir bewusst. Vernunft fängt wieder an zu sprechen Und
+          Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So
+          schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der
+          Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur
+          Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Es irrt der
+          Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Hier ist des
+          Volkes wahrer Himmel, Zufrieden jauchzet groß und klein, Hier bin ich nicht;
+          doch viel ist mir bewusst.
+        </p>
+        <p>
+          So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der
+          Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur
+          Hölle!So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis
+          der Schöpfung aus, Und wandelt mit bedächt'ger Schnelle Vom Himmel durch die
+          Welt zur Hölle. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu
+          sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu sprechen
+          Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt
+          sich nach des Lebens goldner Baum. Es irrt der Mensch, wenn er sie beim Kragen
+          hätte.
+        </p>
+        <p>
+          So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der
+          Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur
+          Hölle! Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
