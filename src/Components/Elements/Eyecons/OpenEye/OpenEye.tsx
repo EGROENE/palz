9e58f7730a022c1d9cd873d43fdaa05495cc14cb
Ethan Groene <ethangroene@gmail.com>
2025-04-02 12:12:40 -0600

    Add keyDown Enter functionality

--- src/Components/Elements/Eyecons/OpenEye/OpenEye.tsx
+++ src/Components/Elements/Eyecons/OpenEye/OpenEye.tsx
@@ -3,6 +3,13 @@ import styles from "../styles.module.css";
 const OpenEye = ({ toggleHidePassword }: { toggleHidePassword: () => void }) => {
   return (
     <i
+      tabIndex={0}
+      aria-hidden="false"
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          toggleHidePassword();
+        }
+      }}
       onClick={() => toggleHidePassword()}
       className={`${styles.eye} fas fa-eye-slash`}
       title="Hide Password"
