b42f9c95a376b11c213611e145feafa237bfe6ff
Ethan Groene <ethangroene@gmail.com>
2025-04-14 16:19:22 -0600

    Add MobileNavOptions component
    This is a list of site-navigation options intended to be used on devices w/ screen width 1024px or less.

---
+++ src/Components/Elements/MobileNavOptions/MobileNavOptions.tsx
@@ -0,0 +1,115 @@
+import { Link } from "react-router-dom";
+import { useMainContext } from "../../../Hooks/useMainContext";
+import { useUserContext } from "../../../Hooks/useUserContext";
+import { useChatContext } from "../../../Hooks/useChatContext";
+import { TChat } from "../../../types";
+
+const MobileNavOptions = () => {
+  const { setShowMobileNavOptions, currentRoute } = useMainContext();
+  const { currentUser, userCreatedAccount, logout, friendRequestsReceived } =
+    useUserContext();
+  const { getTotalNumberOfUnreadMessages, fetchChatsQuery } = useChatContext();
+
+  const userChats: TChat[] | undefined = fetchChatsQuery.data;
+
+  const totalUnreadUserMessages: string | number | undefined = userChats
+    ? getTotalNumberOfUnreadMessages(userChats)
+    : undefined;
+
+  return (
+    <div className="mobile-nav-options">
+      <i
+        title="Close"
+        onClick={() => setShowMobileNavOptions(false)}
+        className="fas fa-times close-module-icon"
+      ></i>
+      <div className="mobile-nav-options-container">
+        {currentRoute !== "/add-event" && (
+          <Link onClick={() => setShowMobileNavOptions(false)} to={"/add-event"}>
+            Create Event
+          </Link>
+        )}
+        {currentRoute !== `/${currentUser?.username}/events` && (
+          <Link
+            onClick={() => setShowMobileNavOptions(false)}
+            to={`/${currentUser?.username}/events`}
+          >
+            My Events
+          </Link>
+        )}
+        {currentRoute !== "/events" && (
+          <Link onClick={() => setShowMobileNavOptions(false)} to={"/events"}>
+            Explore Events
+          </Link>
+        )}
+        {currentRoute !== `/${currentUser?.username}/friend-requests` && (
+          <Link
+            onClick={() => setShowMobileNavOptions(false)}
+            to={`/${currentUser?.username}/friend-requests`}
+          >
+            {
+              <>
+                Friend Requests{" "}
+                {friendRequestsReceived && friendRequestsReceived.length > 0 && (
+                  <span className="notifications-count">
+                    {friendRequestsReceived.length}
+                  </span>
+                )}
+              </>
+            }
+          </Link>
+        )}
+        {currentRoute !== "/find-palz" && (
+          <Link onClick={() => setShowMobileNavOptions(false)} to="/find-palz">
+            Find Palz
+          </Link>
+        )}
+        {currentRoute !== "/my-palz" && (
+          <Link onClick={() => setShowMobileNavOptions(false)} to={"/my-palz"}>
+            My Palz
+          </Link>
+        )}
+        {currentRoute !== "/chats" && (
+          <>
+            <Link onClick={() => setShowMobileNavOptions(false)} to={"/chats"}>
+              Chats
+            </Link>
+            {totalUnreadUserMessages !== 0 && (
+              <span className="notifications-count">{totalUnreadUserMessages}</span>
+            )}
+          </>
+        )}
+        {currentRoute !== "/settings" && (
+          <Link onClick={() => setShowMobileNavOptions(false)} to={"/settings"}>
+            Settings
+          </Link>
+        )}
+        {currentRoute !== "/terms-and-conditions" && (
+          <Link
+            onClick={() => setShowMobileNavOptions(false)}
+            to={"/terms-and-conditions"}
+          >
+            Terms & Conditions
+          </Link>
+        )}
+        {userCreatedAccount !== null ? (
+          <div
+            onClick={() => {
+              logout();
+              setShowMobileNavOptions(false);
+            }}
+          >
+            Log Out
+            <i style={{ marginLeft: "0.5rem" }} className="fas fa-sign-out-alt"></i>
+          </div>
+        ) : (
+          <Link to="/">
+            Log In/Sign Up<i className="fas fa-sign-out-alt"></i>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MobileNavOptions;
