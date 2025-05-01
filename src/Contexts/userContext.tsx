101e8e232ddffe3d59bf446ca1a8a45be5d49923
Ethan Groene <ethangroene@gmail.com>
2025-05-01 10:41:27 -0600

    Call hRecFRFail if responses not ok
    ...instead of in .catch or onError

--- src/Contexts/userContext.tsx
+++ src/Contexts/userContext.tsx
@@ -429,35 +429,37 @@ export const UserContextProvider = ({ children }: { children: ReactNode }) => {
       if (data.ok) {
         if (currentUser && currentUser._id) {
           Requests.getUserByID(currentUser._id)
-            .then((res) =>
-              res
-                .json()
-                .then((user) => {
-                  setCurrentUser(user);
-                  toast.success("Friend request sent!", {
-                    style: {
-                      background: theme === "light" ? "#242424" : "rgb(233, 231, 228)",
-                      color: theme === "dark" ? "black" : "white",
-                      border: "2px solid green",
-                    },
-                  });
-                })
-                .catch((error) => {
-                  console.log(error);
-                  handleReceiveFriendRequestFail(variables);
-                })
-            )
-            .catch((error) => {
-              console.log(error);
-              handleReceiveFriendRequestFail(variables);
-            });
+            .then((res) => {
+              if (res.ok) {
+                res
+                  .json()
+                  .then((user) => {
+                    if (user) {
+                      setCurrentUser(user);
+                      toast.success("Friend request sent!", {
+                        style: {
+                          background:
+                            theme === "light" ? "#242424" : "rgb(233, 231, 228)",
+                          color: theme === "dark" ? "black" : "white",
+                          border: "2px solid green",
+                        },
+                      });
+                    } else {
+                      handleReceiveFriendRequestFail(variables);
+                    }
+                  })
+                  .catch((error) => console.log(error));
+              } else {
+                handleReceiveFriendRequestFail(variables);
+              }
+            })
+            .catch((error) => console.log(error));
         }
+      } else {
+        handleReceiveFriendRequestFail(variables);
       }
     },
-    onError: (error, variables) => {
-      console.log(error);
-      handleReceiveFriendRequestFail(variables);
-    },
+    onError: (error) => console.log(error),
     onSettled: () => setIsLoading(false),
   });
 
