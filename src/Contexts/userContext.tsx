a49eefc4430046a9b88ce582ea0e6fcf42a270b6
Ethan Groene <ethangroene@gmail.com>
2025-05-07 23:22:40 -0600

    getUserById,then use as param in addToFRSent

--- src/Contexts/userContext.tsx
+++ src/Contexts/userContext.tsx
@@ -405,8 +405,14 @@ export const UserContextProvider = ({ children }: { children: ReactNode }) => {
   Success of a sent friend request depends on if both of the mutations below are successful, so call second mutation in onSuccess of mutation that runs first. Display toast of send-friend-request success or failure in onSuccess/onError of second mutation that runs. setIsLoading(false) upon settling of second mutation that runs.
   */
   const sendFriendRequestMutation = useMutation({
-    mutationFn: ({ sender, recipient }: { sender: TUser; recipient: TOtherUser }) =>
-      Requests.addToFriendRequestsSent(sender, recipient),
+    mutationFn: ({ sender, recipient }: { sender: TUser; recipient: TOtherUser }) => {
+      // @ts-ignore: recipient._id will be defined as long as recipient is defined. Ignore TS error b/c, if logic to check if recipient._id is put into mutationFn, an error on mutationFn occurs, since something will only be returned if recipient._id is defined.
+      return Requests.getUserByID(recipient._id).then((res) =>
+        res.json().then((recipient) => {
+          return Requests.addToFriendRequestsSent(sender, recipient);
+        })
+      );
+    },
     onSuccess: (data, variables) => {
       if (data.ok) {
         const sender = variables.sender;
