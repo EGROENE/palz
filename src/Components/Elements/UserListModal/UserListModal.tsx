872ab94fd893b58709c03c8d19f143d000efd9d1
Ethan Groene <ethangroene@gmail.com>
2025-05-08 16:08:54 -0600

    Def,incorporate visibleOtherUsers
    This is set to fetchAllVisibleOtherUsersQuery.data. Since blocked/ing users are already not included in this,no logic needs to be done in this component to filter them out.

--- src/Components/Elements/UserListModal/UserListModal.tsx
+++ src/Components/Elements/UserListModal/UserListModal.tsx
@@ -1,5 +1,5 @@
 import { useUserContext } from "../../../Hooks/useUserContext";
-import { TUser } from "../../../types";
+import { TOtherUser, TUser } from "../../../types";
 import ListedUser from "../ListedUser/ListedUser";
 import { useEventContext } from "../../../Hooks/useEventContext";
 import { useMainContext } from "../../../Hooks/useMainContext";
@@ -52,22 +52,24 @@ const UserListModal = ({
 }) => {
   const { isLoading } = useMainContext();
   const {
-    allUsers,
     currentUser,
     blockedUsers,
     setBlockedUsers,
     fetchAllVisibleOtherUsersQuery,
     handleUnblockUser,
   } = useUserContext();
+
+  const visibleOtherUsers = fetchAllVisibleOtherUsersQuery.data;
+
   const { currentEvent, fetchAllEventsQuery, handleDeleteUserRSVP, handleRemoveInvitee } =
     useEventContext();
   const { startConversation } = useChatContext();
 
-  const getUserArray = (): TUser[] => {
-    const userArray: TUser[] = [];
-    if (userIDArray && allUsers) {
+  const getUserArray = (): TOtherUser[] => {
+    const userArray: TOtherUser[] = [];
+    if (userIDArray && visibleOtherUsers) {
       for (const userID of userIDArray) {
-        const matchingUser = allUsers.filter((user) => user._id === userID)[0];
+        const matchingUser = visibleOtherUsers.filter((user) => user._id === userID)[0];
         userArray.push(matchingUser);
       }
     }
@@ -75,26 +77,7 @@ const UserListModal = ({
   };
   const userArray = getUserArray();
 
-  const getNumberOfUsersInvisibleToCurrentUser = (): number => {
-    let sum = 0;
-    if (listType !== "blocked-users") {
-      for (const user of userArray) {
-        if (user && user._id && currentUser && currentUser._id) {
-          if (
-            user.blockedUsers.includes(currentUser._id) ||
-            currentUser?.blockedUsers.includes(user._id) ||
-            user._id === currentUser._id
-          ) {
-            sum++;
-          }
-        }
-      }
-    }
-    return sum;
-  };
-  const numberOfUsersInvisibleToCurrentUser = getNumberOfUsersInvisibleToCurrentUser();
-
-  const displayedUserCount = userArray.length - numberOfUsersInvisibleToCurrentUser;
+  const displayedUserCount = userArray.length;
 
   const getButtonOneHandlerParams = (user: TUser) => {
     if (!buttonOneHandlerParams) {
@@ -155,24 +138,6 @@ const UserListModal = ({
   };
   const queryForQueryLoadingOrError = getQueryForQueryLoadingOrErrorComponent();
 
-  const getListedUserIsVisible = (user: TUser): boolean => {
-    if (listType !== "blocked-users") {
-      if (user && user._id && currentUser && currentUser._id) {
-        if (
-          user.blockedUsers.includes(currentUser._id) ||
-          currentUser?.blockedUsers.includes(user._id)
-        ) {
-          return false;
-        }
-      }
-    }
-
-    if (user._id === currentUser?._id) {
-      return false;
-    }
-    return true;
-  };
-
   return (
     <div tabIndex={0} aria-hidden="false" className="modal-background">
       <i
@@ -192,38 +157,35 @@ const UserListModal = ({
         {isNoFetchError &&
           !fetchIsLoading &&
           (displayedUserCount > 0 ? (
-            userArray.map(
-              (user) =>
-                getListedUserIsVisible(user) && (
-                  <ListedUser
-                    key={user._id}
-                    renderButtonOne={renderButtonOne}
-                    renderButtonTwo={renderButtonTwo}
-                    user={user}
-                    buttonOneText={buttonOneText}
-                    buttonOneHandler={buttonOneHandler}
-                    buttonOneHandlerNeedsEventParam={buttonOneHandlerNeedsEventParam}
-                    buttonOneHandlerParams={
-                      buttonOneHandlerParams
-                        ? buttonOneHandlerParams
-                        : getButtonOneHandlerParams(user)
-                    }
-                    buttonOneLink={buttonOneLink ? buttonOneLink : getButtonOneLink(user)}
-                    buttonOneIsDisabled={isLoading}
-                    buttonTwoText={buttonTwoText}
-                    buttonTwoIsDisabled={isLoading}
-                    buttonTwoHandler={buttonTwoHandler}
-                    buttonTwoHandlerNeedsEventParam={buttonTwoHandlerNeedsEventParam}
-                    buttonTwoHandlerParams={
-                      buttonTwoHandlerParams
-                        ? buttonTwoHandlerParams
-                        : getButtonTwoHandlerParams(user)
-                    }
-                    buttonTwoLink={buttonTwoLink ? buttonTwoLink : null}
-                    objectLink={`/users/${user?.username}`}
-                  />
-                )
-            )
+            userArray.map((user) => (
+              <ListedUser
+                key={user._id}
+                renderButtonOne={renderButtonOne}
+                renderButtonTwo={renderButtonTwo}
+                user={user}
+                buttonOneText={buttonOneText}
+                buttonOneHandler={buttonOneHandler}
+                buttonOneHandlerNeedsEventParam={buttonOneHandlerNeedsEventParam}
+                buttonOneHandlerParams={
+                  buttonOneHandlerParams
+                    ? buttonOneHandlerParams
+                    : getButtonOneHandlerParams(user)
+                }
+                buttonOneLink={buttonOneLink ? buttonOneLink : getButtonOneLink(user)}
+                buttonOneIsDisabled={isLoading}
+                buttonTwoText={buttonTwoText}
+                buttonTwoIsDisabled={isLoading}
+                buttonTwoHandler={buttonTwoHandler}
+                buttonTwoHandlerNeedsEventParam={buttonTwoHandlerNeedsEventParam}
+                buttonTwoHandlerParams={
+                  buttonTwoHandlerParams
+                    ? buttonTwoHandlerParams
+                    : getButtonTwoHandlerParams(user)
+                }
+                buttonTwoLink={buttonTwoLink ? buttonTwoLink : null}
+                objectLink={`/users/${user?.username}`}
+              />
+            ))
           ) : (
             <p>No users to show</p>
           ))}
