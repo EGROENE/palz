7ad6b68d9d5c9575a7e92742b043ba29fc62024a
Ethan Groene <ethangroene@gmail.com>
2025-05-12 16:30:28 -0600

    Connect cont to check edited user info,to route

--- backend/routes/users.cjs
+++ backend/routes/users.cjs
@@ -8,12 +8,16 @@ const {
   deleteUser,
   updateUser,
   getUserByUsernameOrEmailAddress,
+  getUserByUsernamePhoneNumberOrEmailAddress,
 } = require("../controllers/userControllers.js");
 
 const router = express.Router();
 
 // GET all users
 router.get("/", getAllUsers);
+
+// Used to check if username, email, phone number are unique when saving Settings form
+router.post("/settings", getUserByUsernamePhoneNumberOrEmailAddress);
 
 // Get single user by username or email (used to set currentUser when user logs in):
 // Is post request, since GET cannot have a request body
