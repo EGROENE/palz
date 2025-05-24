7a4bbc687490bb3ee2d505ecb27d5dd5f6e637ac
Ethan Groene <ethangroene@gmail.com>
2025-05-24 12:48:46 -0400

    Del unneeded quotation mark from err msg

--- src/Components/Pages/UserHomepage/UserHomepage.tsx
+++ src/Components/Pages/UserHomepage/UserHomepage.tsx
@@ -97,7 +97,7 @@ const UserHomepage = () => {
       {fetchAllEventsQuery.isError ||
         (fetchAllVisibleOtherUsersQuery.isError && (
           <div className="query-error-container">
-            <header className="query-status-text">"Error fetching data.</header>
+            <header className="query-status-text">Error fetching data.</header>
             <div className="theme-element-container">
               <button onClick={() => window.location.reload()}>Retry</button>
             </div>
