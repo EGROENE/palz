d5120ba100f70084a83a2ff7452b7d41a9588830
Ethan Groene <ethangroene@gmail.com>
2025-05-30 11:11:46 -0400

    Conditionally render links on pg bottom

--- src/Components/Elements/SiteLinks/SiteLinks.tsx
+++ src/Components/Elements/SiteLinks/SiteLinks.tsx
@@ -1,36 +1,48 @@
 import { Link } from "react-router-dom";
 import { useUserContext } from "../../../Hooks/useUserContext";
+import { useMainContext } from "../../../Hooks/useMainContext";
 
 const SiteLinks = () => {
+  const { currentRoute } = useMainContext();
   const { currentUser } = useUserContext();
 
   return (
     <div className="site-links-container">
-      <Link to={"/add-event"}>
-        <div className="theme-element-container">
-          <button>Create Event</button>
-        </div>
-      </Link>
-      <Link to={"/events"}>
-        <div className="theme-element-container">
-          <button>Explore Events</button>
-        </div>
-      </Link>
-      <Link to={`/find-palz`}>
-        <div className="theme-element-container">
-          <button>Find Palz</button>
-        </div>
-      </Link>
-      <Link to={`/${currentUser?.username}/events`}>
-        <div className="theme-element-container">
-          <button>My Events</button>
-        </div>
-      </Link>
-      <Link to={"/my-palz"}>
-        <div className="theme-element-container">
-          <button>My Palz</button>
-        </div>
-      </Link>
+      {currentRoute !== "/add-event" && (
+        <Link to={"/add-event"}>
+          <div className="theme-element-container">
+            <button>Create Event</button>
+          </div>
+        </Link>
+      )}
+      {currentRoute !== "/events" && (
+        <Link to={"/events"}>
+          <div className="theme-element-container">
+            <button>Explore Events</button>
+          </div>
+        </Link>
+      )}
+      {currentRoute !== "/find-palz" && (
+        <Link to="/find-palz">
+          <div className="theme-element-container">
+            <button>Find Palz</button>
+          </div>
+        </Link>
+      )}
+      {currentRoute !== `/${currentUser?.username}/events` && (
+        <Link to={`/${currentUser?.username}/events`}>
+          <div className="theme-element-container">
+            <button>My Events</button>
+          </div>
+        </Link>
+      )}
+      {currentRoute !== "/my-palz" && (
+        <Link to={"/my-palz"}>
+          <div className="theme-element-container">
+            <button>My Palz</button>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
