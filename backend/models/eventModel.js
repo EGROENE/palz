29d7f51d6b5c8317d3fcd2adbbc9aa278f4e3210
Ethan Groene <ethangroene@gmail.com>
2025-06-03 20:46:05 -0400

    Add index field to eventSchema

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+  index: {
+    type: Number,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
