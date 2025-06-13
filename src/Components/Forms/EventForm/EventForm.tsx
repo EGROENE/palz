6a29c26f6b5d53ae820aa03034e62faadd77fc62
Ethan Groene <ethangroene@gmail.com>
2025-06-13 17:30:42 -0400

    Ensure correct form vals render
    currentEvent is set, then field values are set to values on currentEvent

--- src/Components/Forms/EventForm/EventForm.tsx
+++ src/Components/Forms/EventForm/EventForm.tsx
@@ -185,14 +185,16 @@ const EventForm = ({
       setShowSidebar(false);
     }
 
+    setCurrentEvent(event);
+
     if (usedFor === "add-event") {
       if (eventImages && eventImages.length > 0) {
         // Remove any previously added event images (like if user added some on new event, but didn't submit form)
         setEventImages([]);
       }
       setCurrentEvent(undefined);
-      handleRevert();
     }
+    handleRevert();
   }, [usedFor]);
 
   // useEffect to fetch more users when fetch starts change
