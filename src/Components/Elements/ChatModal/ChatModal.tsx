be8827465d83c659f7462dad9db8402861f28adf
Ethan Groene <ethangroene@gmail.com>
2025-06-17 18:04:56 -0400

    Style fontFamily of header

--- src/Components/Elements/ChatModal/ChatModal.tsx
+++ src/Components/Elements/ChatModal/ChatModal.tsx
@@ -768,7 +768,7 @@ const ChatModal = () => {
           className="add-members-modal-container"
         >
           <div className="add-members-modal">
-            <header>Add people to chat:</header>
+            <header style={{ fontFamily: "var(--text-font" }}>Add people to chat:</header>
             {usersToAddToChat.length > 0 && (
               <div className="added-user-tab-container">
                 {usersToAddToChat.map((user) => (
