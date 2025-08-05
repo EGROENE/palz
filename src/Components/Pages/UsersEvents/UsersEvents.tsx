acee99efa668046d206f50634dcc8bdc3c604d0d
Ethan Groene <ethangroene@gmail.com>
2025-08-04 19:12:31 -0600

    Get types of currentUser evs by reqs
    ...instead of using fetchAllEventsQuery & fetchAllVisibleOtherUsersQuery. Loading & errors are handled accordingly.

--- src/Components/Pages/UsersEvents/UsersEvents.tsx
+++ src/Components/Pages/UsersEvents/UsersEvents.tsx
@@ -7,29 +7,60 @@ import { TEvent, TThemeColor } from "../../../types";
 import UserEventsSection from "../../Elements/UserEventsSection/UserEventsSection";
 import toast from "react-hot-toast";
 import { useEventContext } from "../../../Hooks/useEventContext";
+import Requests from "../../../requests";
 
 const UsersEvents = () => {
   const { showSidebar, setShowSidebar, theme } = useMainContext();
-  const { currentUser, userCreatedAccount, fetchAllVisibleOtherUsersQuery, logout } =
-    useUserContext();
-  const { fetchAllEventsQuery, setCurrentEvent } = useEventContext();
 
-  const allEvents = fetchAllEventsQuery.data;
+  const { currentUser, userCreatedAccount, logout } = useUserContext();
 
-  const navigation = useNavigate();
-  useEffect(() => {
-    if (!currentUser || userCreatedAccount === null) {
-      logout();
-      setCurrentEvent(undefined);
-      toast.error("Please log in before accessing this page", {
-        style: {
-          background: theme === "light" ? "#242424" : "rgb(233, 231, 228)",
-          color: theme === "dark" ? "black" : "white",
-          border: "2px solid red",
-        },
-      });
-    }
-  }, [currentUser, navigation, userCreatedAccount]);
+  const { setCurrentEvent } = useEventContext();
+
+  const [ongoingEvents, setOngoingEvents] = useState<TEvent[] | null>(null);
+  const [fetchOngoingEventsIsLoading, setFetchOngoingEventsIsLoading] =
+    useState<boolean>(true);
+
+  const [upcomingEventsUserRSVPdTo, setUpcomingEventsUserRSVPdTo] = useState<
+    TEvent[] | null
+  >(null);
+  const [
+    fetchUpcomingEventsUserRSVPdToIsLoading,
+    setFetchUpcomingEventsUserRSVPdToIsLoading,
+  ] = useState<boolean>(true);
+
+  const [upcomingEventsUserOrganizes, setUpcomingEventsUserOrganizes] = useState<
+    TEvent[] | null
+  >(null);
+  const [
+    fetchUpcomingEventsUserOrganizesIsLoading,
+    setFetchUpcomingEventsUserOrganizesIsLoading,
+  ] = useState<boolean>(true);
+
+  const [recentEventsUserRSVPdTo, setRecentEventsUserRSVPdTo] = useState<TEvent[] | null>(
+    null
+  );
+  const [
+    fetchRecentEventsUserRSVPdToIsLoading,
+    setFetchRecentEventsUserRSVPdToIsLoading,
+  ] = useState<boolean>(true);
+
+  const [upcomingEventsUserInvitedTo, setUpcomingEventsUserInvitedTo] = useState<
+    TEvent[] | null
+  >(null);
+  const [
+    fetchUpcomingEventsUserInvitedToIsLoading,
+    setFetchUpcomingEventsUserInvitedToIsLoading,
+  ] = useState<boolean>(true);
+
+  const [recentEventsUserOrganized, setRecentEventsUserOrganized] = useState<
+    TEvent[] | null
+  >(null);
+  const [
+    fetchRecentEventsUserOrganizedIsLoading,
+    setFetchRecentEventsUserOrganizedIsLoading,
+  ] = useState<boolean>(true);
+
+  const [isFetchError, setIsFetchError] = useState<boolean>(false);
 
   const [randomColor, setRandomColor] = useState<TThemeColor | undefined>();
 
@@ -50,92 +81,119 @@ const UsersEvents = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  const now = Date.now();
+  const navigation = useNavigate();
+  useEffect(() => {
+    if (!currentUser || userCreatedAccount === null) {
+      logout();
+      setCurrentEvent(undefined);
+      toast.error("Please log in before accessing this page", {
+        style: {
+          background: theme === "light" ? "#242424" : "rgb(233, 231, 228)",
+          color: theme === "dark" ? "black" : "white",
+          border: "2px solid red",
+        },
+      });
+    } else {
+      if (currentUser && currentUser.username) {
+        Requests.getOngoingEvents(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((ongoingEvents: TEvent[]) => {
+                setOngoingEvents(ongoingEvents);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchOngoingEventsIsLoading(false));
+        Requests.getUpcomingEventsUserRSVPdTo(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((evs: TEvent[]) => {
+                setUpcomingEventsUserRSVPdTo(evs);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchUpcomingEventsUserRSVPdToIsLoading(false));
 
-  const pastEventsUserOrganized: TEvent[] | undefined = allEvents?.filter(
-    (event) =>
-      currentUser?._id &&
-      event.creator !== currentUser?._id &&
-      event.organizers.includes(currentUser._id.toString()) &&
-      event.eventEndDateTimeInMS < now
-  );
+        Requests.getUpcomingEventsUserOrganizes(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((evs: TEvent[]) => {
+                setUpcomingEventsUserOrganizes(evs);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchUpcomingEventsUserOrganizesIsLoading(false));
 
-  const pastEventsUserRSVPd: TEvent[] | undefined = allEvents?.filter(
-    (event) =>
-      currentUser?._id &&
-      event.interestedUsers.includes(currentUser._id.toString()) &&
-      event.eventEndDateTimeInMS < now
-  );
+        Requests.getRecentEventsUserRSVPdTo(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((evs: TEvent[]) => {
+                setRecentEventsUserRSVPdTo(evs);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchRecentEventsUserRSVPdToIsLoading(false));
 
-  const upcomingEventsUserOrganizes: TEvent[] | undefined = allEvents?.filter(
-    (event) =>
-      event.eventStartDateTimeInMS > now &&
-      event.eventEndDateTimeInMS > now &&
-      currentUser?._id &&
-      event.organizers.includes(currentUser._id.toString())
-  );
+        Requests.getUpcomingEventsUserInvitedTo(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((evs: TEvent[]) => {
+                setUpcomingEventsUserInvitedTo(evs);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchUpcomingEventsUserInvitedToIsLoading(false));
 
-  const upcomingEventsUserInvitedTo: TEvent[] | undefined = allEvents?.filter(
-    (event) =>
-      event.eventStartDateTimeInMS > now &&
-      event.eventEndDateTimeInMS > now &&
-      currentUser?._id &&
-      event.invitees.includes(currentUser._id.toString())
-  );
+        Requests.getRecentEventsUserOrganized(currentUser.username)
+          .then((res) => {
+            if (res.ok) {
+              res.json().then((evs: TEvent[]) => {
+                setRecentEventsUserOrganized(evs);
+              });
+            } else {
+              setIsFetchError(true);
+            }
+          })
+          .catch((error) => console.log(error))
+          .finally(() => setFetchRecentEventsUserOrganizedIsLoading(false));
+      } else {
+        setIsFetchError(true);
+      }
+    }
+  }, [currentUser, navigation, userCreatedAccount]);
 
-  const upcomingEventsUserRSVPdTo: TEvent[] | undefined = allEvents?.filter(
-    (event) =>
-      event.eventStartDateTimeInMS > now &&
-      event.eventEndDateTimeInMS > now &&
-      currentUser?._id &&
-      event.interestedUsers.includes(currentUser._id.toString())
-  );
-
-  const ongoingEvents: TEvent[] | undefined = allEvents?.filter((event) => {
-    event.eventStartDateTimeInMS < now &&
-      event.eventEndDateTimeInMS > now &&
-      currentUser?._id &&
-      (event.organizers.includes(currentUser._id.toString()) ||
-        event.interestedUsers.includes(currentUser._id.toString()));
-  });
-
-  const usersEvents = [
-    { header: "Events Happening Now", array: ongoingEvents },
-    {
-      header: "Your Upcoming Events",
-      array: upcomingEventsUserOrganizes,
-    },
-    {
-      header: "Upcoming Events You've RSVP'd To",
-      array: upcomingEventsUserRSVPdTo,
-    },
-    {
-      header: "Upcoming Events You've Been Invited To",
-      array: upcomingEventsUserInvitedTo,
-    },
-    { header: "Past Events You RSVP'd To", array: pastEventsUserRSVPd },
-    { header: "Past Events You Organized", array: pastEventsUserOrganized },
-  ];
-
-  const userEventsExist = usersEvents
-    .map((event) => event.array)
-    .some((eventArray) => eventArray && eventArray.length > 0);
-
-  const isNoFetchError: boolean =
-    !fetchAllEventsQuery.isError && !fetchAllVisibleOtherUsersQuery.isError;
+  const userEventsExist: boolean =
+    (ongoingEvents && ongoingEvents.length > 0) ||
+    (upcomingEventsUserRSVPdTo && upcomingEventsUserRSVPdTo.length > 0) ||
+    (upcomingEventsUserOrganizes && upcomingEventsUserOrganizes.length > 0) ||
+    (recentEventsUserRSVPdTo && recentEventsUserRSVPdTo.length > 0) ||
+    (upcomingEventsUserInvitedTo && upcomingEventsUserInvitedTo.length > 0) ||
+    (recentEventsUserOrganized && recentEventsUserOrganized.length > 0)
+      ? true
+      : false;
 
   const fetchIsLoading: boolean =
-    fetchAllEventsQuery.isLoading || fetchAllVisibleOtherUsersQuery.isLoading;
-
-  const getQueryForQueryLoadingOrErrorComponent = () => {
-    if (fetchAllVisibleOtherUsersQuery.isError) {
-      return fetchAllVisibleOtherUsersQuery;
-    } else if (fetchAllEventsQuery.isError) {
-      return fetchAllEventsQuery;
-    }
-    return undefined;
-  };
-  const queryWithError = getQueryForQueryLoadingOrErrorComponent();
+    fetchOngoingEventsIsLoading ||
+    fetchUpcomingEventsUserRSVPdToIsLoading ||
+    fetchUpcomingEventsUserOrganizesIsLoading ||
+    fetchRecentEventsUserRSVPdToIsLoading ||
+    fetchUpcomingEventsUserInvitedToIsLoading ||
+    fetchRecentEventsUserOrganizedIsLoading;
 
   return (
     <>
@@ -145,30 +203,44 @@ const UsersEvents = () => {
           Loading...
         </header>
       )}
-      {queryWithError && (
-        <div className="query-error-container">
-          <header className="query-status-text">Error fetching data.</header>
-          <div className="theme-element-container">
-            <button onClick={() => window.location.reload()}>Retry</button>
-          </div>
-        </div>
+      {!isFetchError && !fetchIsLoading && userEventsExist && (
+        <>
+          {ongoingEvents && ongoingEvents.length > 0 && (
+            <UserEventsSection eventsArray={ongoingEvents} header="Your Ongoing Events" />
+          )}
+          {upcomingEventsUserInvitedTo && upcomingEventsUserInvitedTo.length > 0 && (
+            <UserEventsSection
+              eventsArray={upcomingEventsUserInvitedTo}
+              header="Upcoming Events You've Been Invited To"
+            />
+          )}
+          {upcomingEventsUserRSVPdTo && upcomingEventsUserRSVPdTo.length > 0 && (
+            <UserEventsSection
+              eventsArray={upcomingEventsUserRSVPdTo}
+              header="Upcoming Events You've RSVPd To"
+            />
+          )}
+          {upcomingEventsUserOrganizes && upcomingEventsUserOrganizes.length > 0 && (
+            <UserEventsSection
+              eventsArray={upcomingEventsUserOrganizes}
+              header="Upcoming Events You're Organizing"
+            />
+          )}
+          {recentEventsUserOrganized && recentEventsUserOrganized.length > 0 && (
+            <UserEventsSection
+              eventsArray={recentEventsUserOrganized}
+              header="Upcoming Events You Organized"
+            />
+          )}
+          {recentEventsUserRSVPdTo && recentEventsUserRSVPdTo.length > 0 && (
+            <UserEventsSection
+              eventsArray={recentEventsUserRSVPdTo}
+              header="Recent Events You RSVPd To"
+            />
+          )}
+        </>
       )}
-      {isNoFetchError &&
-        !fetchIsLoading &&
-        userEventsExist &&
-        usersEvents.map(
-          (event) =>
-            event &&
-            event.array &&
-            event.array.length > 0 && (
-              <UserEventsSection
-                key={event.header}
-                eventsArray={event.array}
-                header={event.header}
-              />
-            )
-        )}
-      {isNoFetchError && !fetchIsLoading && !userEventsExist && (
+      {!isFetchError && !fetchIsLoading && !userEventsExist && (
         <>
           <h2>
             Once you create/organize, are invited to, or RSVP to events, they will appear
