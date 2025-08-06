540da729db8144e2939a1c454346258da0594abc
Ethan Groene <ethangroene@gmail.com>
2025-08-06 15:19:33 -0600

    Remove _id from chatSchema
    Causes problems when updating of chat (like when sending a message). Was here b/c _id was manually created when creating a chat to immediately set currentChat,but there's another way to do this.

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -30,10 +30,6 @@ const Schema = mongoose.Schema;
 }); */
 
 const chatSchema = new Schema({
-  _id: {
-    type: String,
-    required: true,
-  },
   members: {
     type: [String],
     required: true,
